9349b01e49979547e20f46b75ef6f539bd2eb7ce
Ghislain Beaulac <ghislain.beaulac@se.com>
2020-03-24 20:14:28 -0400

    refactor(demo): add hamburger menu toggle for mobile device

--- packages/vanilla-bundle-examples/src/app.ts
+++ packages/vanilla-bundle-examples/src/app.ts
@@ -12,7 +12,11 @@ export class App {
     { route: 'example03', name: 'example03', title: 'Example03', moduleId: './examples/example03' },
     { route: 'example50', name: 'example50', title: 'Example50', moduleId: './examples/example50' },
     { route: 'example51', name: 'example51', title: 'Example51', moduleId: './examples/example51' },
-  ]
+  ];
+
+  constructor() {
+    this.navbarHamburgerToggle();
+  }
 
   attached() {
     this.renderer = new Renderer(document.querySelector('view-route'));
@@ -39,5 +43,31 @@ export class App {
       }
       document.title = `${this.documentTitle} · ${mapRoute.name}`;
     }
+  }
+
+  /** Add event listener for the navbar hamburger menu toggle when menu shows up on mobile */
+  navbarHamburgerToggle() {
+    document.addEventListener('DOMContentLoaded', () => {
+
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+          el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+          });
+        });
+      }
+    });
   }
 }
