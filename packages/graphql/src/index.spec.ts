189c1792ebc4a7e99fe7d874dda355ba9579736e
ghiscoding <gbeaulac@gmail.com>
2020-06-15 13:43:02 -0400

    refactor(backend): increase unit test code coverage

--- packages/graphql/src/index.spec.ts
+++ packages/graphql/src/index.spec.ts
@@ -1,12 +1,13 @@
 import * as entry from './index';
 import * as interfaces from './interfaces/index';
-import * as services from './services/index';
+import { GraphqlService, GraphqlQueryBuilder } from './services/index';
 
 describe('Testing GraphQL Package entry point', () => {
   it('should have multiple index entries defined', () => {
     expect(entry).toBeTruthy();
     expect(interfaces).toBeTruthy();
-    expect(services).toBeTruthy();
+    expect(GraphqlService).toBeTruthy();
+    expect(GraphqlQueryBuilder).toBeTruthy();
   });
 
   it('should have 2x Services defined', () => {
