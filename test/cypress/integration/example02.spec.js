ba8d69f99c4d52a9ab11594baf05c5fe923a378a
ghiscoding <gbeaulac@gmail.com>
2020-07-08 17:56:30 -0400

    refactor(footer): add more Cypress E2E tests

--- test/cypress/integration/example02.spec.js
+++ test/cypress/integration/example02.spec.js
@@ -28,13 +28,61 @@ describe('Example 2 - Grid with Formatters', () => {
       .contains('created with Slickgrid-Universal');
   });
 
-  it('should have some metrics shown in the grid footer', () => {
+  it('should have some metrics shown in the grid right footer', () => {
     cy.get('.grid2')
       .find('.slick-custom-footer')
       .find('.right-footer')
       .should($span => {
         const text = removeExtraSpaces($span.text()); // remove all white spaces
         expect(text).to.eq(`Last Update ${moment().format('YYYY-MM-DD, hh:mm a')} | 500 of 500 items`);
+      });
+  });
+
+  it('should type a filter in the Title and expect 176 items shown in the footer', () => {
+    cy.get('.search-filter.filter-title')
+      .clear()
+      .type('2');
+
+    cy.get('.grid2')
+      .find('.slick-custom-footer')
+      .find('.right-footer')
+      .should($span => {
+        const text = removeExtraSpaces($span.text()); // remove all white spaces
+        expect(text).to.eq(`Last Update ${moment().format('YYYY-MM-DD, hh:mm a')} | 176 of 500 items`);
+      });
+  });
+
+  it('should add another filter "Effort-Driven" set to True and expect 28 items shown in the footer', () => {
+    cy.get('div.ms-filter.filter-effortDriven')
+      .trigger('click');
+
+    cy.get('.ms-drop')
+      .find('span:nth(1)')
+      .click();
+
+    cy.get('.grid2')
+      .find('.slick-custom-footer')
+      .find('.right-footer')
+      .should($span => {
+        const text = removeExtraSpaces($span.text()); // remove all white spaces
+        expect(text).to.eq(`Last Update ${moment().format('YYYY-MM-DD, hh:mm a')} | 28 of 500 items`);
+      });
+  });
+
+  it('should change filter "Effort-Driven" to False and expect 148 items shown in the footer', () => {
+    cy.get('div.ms-filter.filter-effortDriven')
+      .trigger('click');
+
+    cy.get('.ms-drop')
+      .find('span:nth(2)')
+      .click();
+
+    cy.get('.grid2')
+      .find('.slick-custom-footer')
+      .find('.right-footer')
+      .should($span => {
+        const text = removeExtraSpaces($span.text()); // remove all white spaces
+        expect(text).to.eq(`Last Update ${moment().format('YYYY-MM-DD, hh:mm a')} | 148 of 500 items`);
       });
   });
 });
