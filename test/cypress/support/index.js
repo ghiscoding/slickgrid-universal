4c53e9bb7ca940aa54cce75ce78ddb1beb7702a0
Ghislain Beaulac <SESA213338@se.com>
2020-11-25 09:54:57 -0500

    refactor: add cypress before unload script

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -18,3 +18,19 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('window:before:load', function (window) {
+  const original = window.EventTarget.prototype.addEventListener;
+
+  window.EventTarget.prototype.addEventListener = function () {
+    if (arguments && arguments[0] === 'beforeunload') {
+      return;
+    }
+    return original.apply(this, arguments);
+  }
+
+  Object.defineProperty(window, 'onbeforeunload', {
+    get: function () { },
+    set: function () { }
+  });
+})
