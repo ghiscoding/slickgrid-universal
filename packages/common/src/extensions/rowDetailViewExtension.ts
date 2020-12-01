e11f23a237d3d814804191038fab918dec8b6c58
ghiscoding <gbeaulac@gmail.com>
2020-12-01 10:07:56 -0500

    refactor: fix failing unit test and build

--- packages/common/src/extensions/rowDetailViewExtension.ts
+++ packages/common/src/extensions/rowDetailViewExtension.ts
@@ -5,7 +5,7 @@ export class RowDetailViewExtension implements Extension {
 
   /** Dispose of the RowDetailView Extension */
   dispose() {
-    throw new Error('[Slickgrid-Universal] Row Detail "dispose" method is not yet implemented');
+    throw new Error('[Slickgrid-Universal] RowDetailViewExtension "dispose" method is not yet implemented');
   }
 
   /**
@@ -13,15 +13,15 @@ export class RowDetailViewExtension implements Extension {
    * Mostly because the column definitions might change after the grid creation
    */
   create(_columnDefinitions: Column[], _gridOptions: GridOption) {
-    throw new Error('[Slickgrid-Universal] Row Detail "create" method is not yet implemented');
+    throw new Error('[Slickgrid-Universal] RowDetailViewExtension "create" method is not yet implemented');
   }
 
   /** Get the instance of the SlickGrid addon (control or plugin). */
   getAddonInstance() {
-    throw new Error('[Slickgrid-Universal] Row Detail "getAddonInstance" method is not yet implemented');
+    throw new Error('[Slickgrid-Universal] RowDetailViewExtension "getAddonInstance" method is not yet implemented');
   }
 
   register(_rowSelectionPlugin?: SlickRowSelectionModel): SlickRowDetailView | null {
-    throw new Error('[Slickgrid-Universal] Row Detail "register" method is not yet implemented');
+    throw new Error('[Slickgrid-Universal] RowDetailViewExtension "register" method is not yet implemented');
   }
 }
