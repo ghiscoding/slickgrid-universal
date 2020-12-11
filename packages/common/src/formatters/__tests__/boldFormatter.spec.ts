a1a87218bb689664cf5d1d44af6c23f6451c0930
ghiscoding <gbeaulac@gmail.com>
2020-12-11 16:43:57 -0500

    refactor: add grid argument to all formatter unit tests

--- packages/common/src/formatters/__tests__/boldFormatter.spec.ts
+++ packages/common/src/formatters/__tests__/boldFormatter.spec.ts
@@ -4,13 +4,13 @@ import { boldFormatter } from '../boldFormatter';
 describe('the Bold Formatter', () => {
   it('should return an empty string when no value is passed', () => {
     const value = null;
-    const result = boldFormatter(0, 0, value, {} as Column, {});
+    const result = boldFormatter(0, 0, value, {} as Column, {}, {} as any);
     expect(result).toBe('');
   });
 
   it('should return a bold html formatted string when value is filled', () => {
     const value = 'john';
-    const result = boldFormatter(0, 0, value, {} as Column, {});
+    const result = boldFormatter(0, 0, value, {} as Column, {}, {} as any);
     expect(result).toBe(`<b>${value}</b>`);
   });
 });
--- packages/common/src/formatters/__tests__/percentCompleteBarWithTextFormatter.spec.ts
+++ packages/common/src/formatters/__tests__/percentCompleteBarWithTextFormatter.spec.ts
@@ -3,26 +3,26 @@ import { percentCompleteBarWithTextFormatter } from '../percentCompleteBarWithTe
 
 describe('the Percent Complete with Text Formatter', () => {
   it('should return an empty string when no value is provided', () => {
-    const output = percentCompleteBarWithTextFormatter(1, 1, '', {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, '', {} as Column, {}, {} as any);
     expect(output).toBe('');
   });
 
   it('should return empty string when non-numeric value is provided', () => {
-    const output = percentCompleteBarWithTextFormatter(1, 1, 'hello', {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, 'hello', {} as Column, {}, {} as any);
     expect(output).toBe('');
   });
 
   it('should display a red color bar formatter when number 0 is provided', () => {
     const input = 0;
     const color = 'red';
-    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {}, {} as any);
     expect(output).toBe(`<div class="percent-complete-bar-with-text" title="${input}%" style="background:${color}; width:${input}%">${input}%</div>`);
   });
 
   it('should display a red color bar when value is a negative number', () => {
     const input = -15;
     const color = 'red';
-    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {}, {} as any);
     expect(output).toBe(`<div class="percent-complete-bar-with-text" title="${input}%" style="background:${color}; width:${input}%">${input}%</div>`);
   });
 
@@ -30,8 +30,8 @@ describe('the Percent Complete with Text Formatter', () => {
     const input1 = 30;
     const input2 = 69;
     const color = 'silver';
-    const output1 = percentCompleteBarWithTextFormatter(1, 1, input1, {} as Column, {});
-    const output2 = percentCompleteBarWithTextFormatter(1, 1, input2, {} as Column, {});
+    const output1 = percentCompleteBarWithTextFormatter(1, 1, input1, {} as Column, {}, {} as any);
+    const output2 = percentCompleteBarWithTextFormatter(1, 1, input2, {} as Column, {}, {} as any);
     expect(output1).toBe(`<div class="percent-complete-bar-with-text" title="${input1}%" style="background:${color}; width:${input1}%">${input1}%</div>`);
     expect(output2).toBe(`<div class="percent-complete-bar-with-text" title="${input2}%" style="background:${color}; width:${input2}%">${input2}%</div>`);
   });
@@ -39,14 +39,14 @@ describe('the Percent Complete with Text Formatter', () => {
   it('should display a green color bar when value greater or equal to 70 and is a type string', () => {
     const input = '70';
     const color = 'green';
-    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {}, {} as any);
     expect(output).toBe(`<div class="percent-complete-bar-with-text" title="${input}%" style="background:${color}; width:${input}%">${input}%</div>`);
   });
 
   it('should display a green color bar with percentage of 100% when number is greater than 100 is provided', () => {
     const input = 125;
     const color = 'green';
-    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {});
+    const output = percentCompleteBarWithTextFormatter(1, 1, input, {} as Column, {}, {} as any);
     expect(output).toBe(`<div class="percent-complete-bar-with-text" title="100%" style="background:${color}; width:100%">100%</div>`);
   });
 });
