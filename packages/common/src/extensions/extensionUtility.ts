b255220ec37dbdc9df4f3ecccb4397656cf9f2a6
Ghislain B <gbeaulac@gmail.com>
2021-01-26 12:05:16 -0500

    fix(frozen): hiding multiple columns when using pinning gets out of sync (#243)

--- packages/common/src/extensions/extensionUtility.ts
+++ packages/common/src/extensions/extensionUtility.ts
@@ -81,6 +81,7 @@ export class ExtensionUtility {
     if (frozenColumnIndex >= 0) {
       const recalculatedFrozenColumnIndex = visibleColumns.findIndex(col => col.id === this.sharedService.frozenVisibleColumnId);
       if (recalculatedFrozenColumnIndex >= 0 && recalculatedFrozenColumnIndex !== frozenColumnIndex) {
+        this.sharedService.gridOptions.frozenColumn = recalculatedFrozenColumnIndex;
         this.sharedService.slickGrid.setOptions({ frozenColumn: recalculatedFrozenColumnIndex });
       }
 
