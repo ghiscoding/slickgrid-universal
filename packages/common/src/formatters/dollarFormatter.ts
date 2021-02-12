d420b694caffdcb39b7835eebc591bc063e96f37
ghiscoding <gbeaulac@gmail.com>
2021-02-12 17:30:26 -0500

    chore(build): use common `isNumber` method from utility set

--- packages/common/src/formatters/dollarFormatter.ts
+++ packages/common/src/formatters/dollarFormatter.ts
@@ -1,17 +1,16 @@
 import { Formatter } from './../interfaces/index';
-import { formatNumber } from './../services/utilities';
+import { formatNumber, isNumber } from './../services/utilities';
 import { getValueFromParamsOrFormatterOptions } from './formatterUtilities';
 
 /** Display the value as 2 decimals formatted with dollar sign '$' at the end of of the value */
 export const dollarFormatter: Formatter = (_row, _cell, value, columnDef, _dataContext, grid) => {
-  const isNumber = (value === null || value === undefined || value === '') ? false : !isNaN(+value);
   const minDecimal = getValueFromParamsOrFormatterOptions('minDecimal', columnDef, grid, 2);
   const maxDecimal = getValueFromParamsOrFormatterOptions('maxDecimal', columnDef, grid, 4);
   const decimalSeparator = getValueFromParamsOrFormatterOptions('decimalSeparator', columnDef, grid, '.');
   const thousandSeparator = getValueFromParamsOrFormatterOptions('thousandSeparator', columnDef, grid, '');
   const displayNegativeNumberWithParentheses = getValueFromParamsOrFormatterOptions('displayNegativeNumberWithParentheses', columnDef, grid, false);
 
-  if (isNumber) {
+  if (isNumber(value)) {
     return formatNumber(value, minDecimal, maxDecimal, displayNegativeNumberWithParentheses, '$', '', decimalSeparator, thousandSeparator);
   }
   return value;
--- packages/common/src/formatters/percentFormatter.ts
+++ packages/common/src/formatters/percentFormatter.ts
@@ -1,17 +1,16 @@
 import { Formatter } from './../interfaces/index';
-import { formatNumber } from './../services/utilities';
+import { formatNumber, isNumber } from './../services/utilities';
 import { getValueFromParamsOrFormatterOptions } from './formatterUtilities';
 
 /** Takes a cell value number (between 0.0-1.0) and displays a red (<50) or green (>=50) bar */
 export const percentFormatter: Formatter = (_row, _cell, value, columnDef, _dataContext, grid) => {
-  const isNumber = (value === null || value === undefined || value === '') ? false : !isNaN(+value);
   const minDecimal = getValueFromParamsOrFormatterOptions('minDecimal', columnDef, grid);
   const maxDecimal = getValueFromParamsOrFormatterOptions('maxDecimal', columnDef, grid);
   const decimalSeparator = getValueFromParamsOrFormatterOptions('decimalSeparator', columnDef, grid, '.');
   const thousandSeparator = getValueFromParamsOrFormatterOptions('thousandSeparator', columnDef, grid, '');
   const displayNegativeNumberWithParentheses = getValueFromParamsOrFormatterOptions('displayNegativeNumberWithParentheses', columnDef, grid, false);
 
-  if (isNumber) {
+  if (isNumber(value)) {
     const percentValue = value * 100;
     return formatNumber(percentValue, minDecimal, maxDecimal, displayNegativeNumberWithParentheses, '', '%', decimalSeparator, thousandSeparator);
   }
