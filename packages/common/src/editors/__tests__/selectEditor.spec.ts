df1f549b2ca8d80d174567b4bc4f691066fef4bf
ghiscoding <gbeaulac@gmail.com>
2021-02-19 20:30:11 -0500

    tests: add selectEditor missing unit test

--- packages/common/src/editors/__tests__/selectEditor.spec.ts
+++ packages/common/src/editors/__tests__/selectEditor.spec.ts
@@ -304,6 +304,14 @@ describe('SelectEditor', () => {
         expect(editor.isValueChanged()).toBe(false);
         expect(editor.isValueTouched()).toBe(true);
       });
+
+      it('should call the "changeEditorOption" method and expect new option to be merged with the previous Editor options and also expect to call MultipleSelect "refreshOptions" setter method', () => {
+        editor = new SelectEditor(editorArguments, true);
+        const multipleSelectSpy = jest.spyOn(editor.editorDomElement, 'multipleSelect');
+        editor.changeEditorOption('filter', true);
+
+        expect(multipleSelectSpy).toHaveBeenCalledWith('refreshOptions', { ...editor.editorElmOptions, filter: true });
+      });
     });
 
     describe('isValueTouched method', () => {
