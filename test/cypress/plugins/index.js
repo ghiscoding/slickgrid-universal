99c7e00667588f219b816074b49d0c75ad6d3550
ghiscoding <gbeaulac@gmail.com>
2021-05-21 19:47:02 -0400

    chore(ci): downgrade Cypress to older version 6.8.0 to fix perf issue - cy.visit always timesout even with 10min wait

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -15,7 +15,34 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+// cypress/plugins/index.js
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      // fullPage screenshot size is 1000x950 on non-retina screens
+      // and 2800x2400 on retina screens
+      launchOptions.args.push('--window-size=1000,950')
+
+      // force screen to be non-retina (1000x950 size)
+      launchOptions.args.push('--force-device-scale-factor=1')
+
+      // force screen to be retina (2800x2400 size)
+      // launchOptions.args.push('--force-device-scale-factor=2')
+    }
+
+    if (browser.name === 'electron' && browser.isHeadless) {
+      // fullPage screenshot size is 1000x950
+      launchOptions.preferences.width = 1000
+      launchOptions.preferences.height = 950
+    }
+
+    if (browser.name === 'firefox' && browser.isHeadless) {
+      // menubars take up height on the screen
+      // so fullPage screenshot size is 1000x1126
+      launchOptions.args.push('--width=1000')
+      launchOptions.args.push('--height=950')
+    }
+
+    return launchOptions
+  })
 }
