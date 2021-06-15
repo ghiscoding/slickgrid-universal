8df9a398a38c173f9a4cb171308deeb8ed428f07
ghiscoding <gbeaulac@gmail.com>
2021-06-15 12:21:22 -0400

    tests: add missing right footer text

--- packages/vanilla-bundle/src/components/__tests__/slick-footer.spec.ts
+++ packages/vanilla-bundle/src/components/__tests__/slick-footer.spec.ts
@@ -348,6 +348,7 @@ describe('Slick-Footer Component', () => {
       expect(footerContainerElm).toBeTruthy();
       expect(rightFooterElm).toBeTruthy();
       expect(rightFooterElm.innerHTML).toBe('initial right footer text');
+      expect(component.rightFooterText).toBe('initial right footer text');
     });
 
     it('should display custom text on the right side footer section when calling the rightFooterText SETTER', () => {
@@ -364,6 +365,7 @@ describe('Slick-Footer Component', () => {
       expect(footerContainerElm).toBeTruthy();
       expect(rightFooterElm).toBeTruthy();
       expect(rightFooterElm.innerHTML).toBe('custom right footer text');
+      expect(component.rightFooterText).toBe('initial right footer text');
     });
   });
 });
