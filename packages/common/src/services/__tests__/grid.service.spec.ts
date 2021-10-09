21e04b37c15c3415a2697392806147536d370167
ghiscoding <gbeaulac@gmail.com>
2021-10-09 09:39:24 -0400

    tests: fix failing unit test

--- packages/common/src/services/__tests__/grid.service.spec.ts
+++ packages/common/src/services/__tests__/grid.service.spec.ts
@@ -14,8 +14,6 @@ const mockSelectionModel = {
 const mockSelectionModelImplementation = jest.fn().mockImplementation(() => mockSelectionModel);
 
 jest.mock('flatpickr', () => { });
-jest.mock('slickgrid/plugins/slick.rowselectionmodel', () => mockSelectionModelImplementation);
-Slick.RowSelectionModel = mockSelectionModelImplementation;
 
 const filterServiceStub = {
   clearFilters: jest.fn(),
@@ -95,6 +93,9 @@ const treeDataServiceStub = {
 } as unknown as TreeDataService;
 
 describe('Grid Service', () => {
+  jest.mock('slickgrid/plugins/slick.rowselectionmodel', () => mockSelectionModelImplementation);
+  Slick.RowSelectionModel = mockSelectionModelImplementation;
+
   let service: GridService;
   const sharedService = new SharedService();
   const mockGridOptions = { enableAutoResize: true } as GridOption;
