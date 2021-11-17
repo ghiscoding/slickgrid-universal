22ed2313c45587f2ebdb279c9e47df881c6f83d6
ghiscoding <gbeaulac@gmail.com>
2021-11-17 09:53:35 -0500

    fix(translation): add new UNFREEZE_COLUMNS to fix translation

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -63,6 +63,7 @@ export class Constants {
     TEXT_STARTS_WITH: 'Starts With',
     TEXT_TOGGLE_FILTER_ROW: 'Toggle Filter Row',
     TEXT_TOGGLE_PRE_HEADER_ROW: 'Toggle Pre-Header Row',
+    TEXT_UNFREEZE_COLUMNS: 'Unfreeze Columns',
     TEXT_X_OF_Y_SELECTED: '# of % selected',
     TEXT_X_OF_Y_MASS_SELECTED: '{{x}} of {{y}} selected',
   };
