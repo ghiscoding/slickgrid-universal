65eecca4003c82640b2614abf3c850ab68114e41
ghiscoding <gbeaulac@gmail.com>
2023-02-10 18:57:23 -0500

    chore: remove commented code

--- packages/common/build-watch.mjs
+++ packages/common/build-watch.mjs
@@ -1,9 +1,8 @@
+import { exec, execSync } from 'child_process';
 import copyfiles from 'copyfiles';
 import path from 'path';
-// import sass from 'sass';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { exec, execSync } from 'child_process';
 
 /**
  * Special script used by the Watch in Development which will compile TypeScript files with tsc incremental and/or SASS files when changes occurs.
@@ -41,36 +40,6 @@ async function run() {
       );
     }
   }
-
-  // for (const changedFile of changedFiles) {
-  //   const extension = path.extname(changedFile);
-  //   if (extension === '.ts') {
-  //     exec('npm run sass-build-task:scss-compile:bootstrap');
-  //   } else if (extension === '.scss') {
-  //     const fileWithExtension = path.basename(changedFile);
-  //     const filename = path.basename(changedFile, '.scss');
-  //     const relativeFile = `src/styles/${fileWithExtension}`;
-  //     // const compressed = sass.compile(relativeFile, { style: 'compressed', quietDeps: true, noSourceMap: true });
-  //     // fs.writeFileSync(`dist/styles/css/${filename}.css`, compressed.css);
-
-  //     // const themeFilenames = ['slickgrid-theme-bootstrap', 'slickgrid-theme-material', 'slickgrid-theme-salesforce'];
-  //     // for (const themeFilename of themeFilenames) {
-  //     //   fs.outputFileSync(
-  //     //     `dist/styles/css/${themeFilename}.css`,
-  //     //     sass.compile(`src/styles/${themeFilename}.scss`, { style: 'compressed', quietDeps: true, noSourceMap: true }).css
-  //     //   );
-  //     // }
-
-  //     await compileAllSassThemes();
-  //     console.log('All Slickgrid-Universal themes successfully compiled to CSS');
-
-  //     copyfiles(
-  //       [relativeFile, 'dist/styles/sass'],
-  //       { up: true },
-  //       (err) => { err ? console.error(err) : console.log(`Copied "${fileWithExtension}" to "dist/styles/sass"`) }
-  //     );
-  //   }
-  // }
 }
 
 async function compileAllSassThemes() {
