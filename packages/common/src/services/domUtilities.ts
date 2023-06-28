ghiscoding/slickgrid-universal

commit 4e3e1d394247be75d1717feece833e200fce21dc

feat!: drop jQuery requirement & use multiple-select-vanilla dependency (#976)
* feat(common)!: migrate to multiple-select-vanilla (#919)

* feat(common)!: migrate to multiple-select-vanilla
- switch from `multiple-select-modified` to [multiple-select-vanilla](https://github.com/ghiscoding/multiple-select-vanilla) which no longer requires jQuery
- might release as a breaking change version since there is a very small change for the CSS import of the new lib, the MultipleSelectOption also no longer exist in Slickgrid-Universal but is instead in the new lib

packages/common/src/services/domUtilities.ts
@@ -1,3 +1,4 @@
+import { OptionRowData } from 'multiple-select-vanilla';
 import * as DOMPurify_ from 'dompurify';
 const DOMPurify = ((DOMPurify_ as any)?.['default'] ?? DOMPurify_); // patch for rollup
 
@@ -16,7 +17,7 @@ import type { TranslaterService } from './translater.service';
  * @param {Array<*>} searchTerms - optional array of search term (used by the "filter" type only)
  * @returns object with 2 properties for the select element & a boolean value telling us if any of the search terms were found and selected in the dropdown
  */
-export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', collection: any[], columnDef: Column, grid: SlickGrid, isMultiSelect = false, translaterService?: TranslaterService, searchTerms?: SearchTerm[]): { selectElement: HTMLSelectElement; hasFoundSearchTerm: boolean; } {
+export function buildMultipleSelectDataCollection(type: 'editor' | 'filter', collection: any[], columnDef: Column, grid: SlickGrid, isMultiSelect = false, translaterService?: TranslaterService, searchTerms?: SearchTerm[]): { selectElement: HTMLSelectElement; dataCollection: OptionRowData[]; hasFoundSearchTerm: boolean; } {
   const columnId = columnDef?.id ?? '';
   const gridOptions = grid.getOptions();
   const columnFilterOrEditor = (type === 'editor' ? columnDef?.internalColumnEditor : columnDef?.filter) ?? {};
@@ -37,38 +38,32 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
   selectElement.classList.add(...extraCssClasses);
 
   selectElement.multiple = isMultiSelect;
-
-  // use an HTML Fragment for performance reason, MDN explains it well as shown below::
-  // The key difference is that because the document fragment isn't part of the actual DOM's structure, changes made to the fragment don't affect the document, cause reflow, or incur any performance impact that can occur when changes are made.
-  const selectOptionsFragment = document.createDocumentFragment();
-
+  const dataCollection: OptionRowData[] = [];
   let hasFoundSearchTerm = false;
 
   // collection could be an Array of Strings OR Objects
   if (Array.isArray(collection)) {
-    if (collection.every((x: any) => typeof x === 'string')) {
+    if (collection.every((x: any) => typeof x === 'number' || typeof x === 'string')) {
       for (const option of collection) {
-        const selectOptionElm = createDomElement('option', {
-          label: option, value: option, textContent: option,
-        });
+        const selectOption: OptionRowData = { text: option, value: option };
         if (type === 'filter' && Array.isArray(searchTerms)) {
-          selectOptionElm.selected = (searchTerms.findIndex(term => term === option) >= 0); // when filter search term is found then select it in dropdown
+          selectOption.selected = (searchTerms.findIndex(term => term === option) >= 0); // when filter search term is found then select it in dropdown
         }
-        selectOptionsFragment.appendChild(selectOptionElm);
+        dataCollection.push(selectOption);
 
         // if there's at least 1 Filter search term found, we will add the "filled" class for styling purposes
         // on a single select, we'll also make sure the single value is not an empty string to consider this being filled
-        if ((selectOptionElm.selected && isMultiSelect) || (selectOptionElm.selected && !isMultiSelect && option !== '')) {
+        if ((selectOption.selected && isMultiSelect) || (selectOption.selected && !isMultiSelect && option !== '')) {
           hasFoundSearchTerm = true;
         }
       }
     } else {
       // array of objects will require a label/value pair unless a customStructure is passed
       collection.forEach((option: SelectOption) => {
-        if (!option || (option[labelName] === undefined && option.labelKey === undefined)) {
+        if (option === undefined || (typeof option === 'object' && option[labelName] === undefined && option.labelKey === undefined)) {
           throw new Error(`[Slickgrid-Universal] Select Filter/Editor collection with value/label (or value/labelKey when using Locale) is required to populate the Select list, for example:: { filter: model: Filters.multipleSelect, collection: [ { value: '1', label: 'One' } ]')`);
         }
-        const selectOptionElm = document.createElement('option');
+
         const labelKey = (option.labelKey || option[labelName]) as string;
         const labelText = ((option.labelKey || (enableTranslateLabel && translaterService)) && labelKey && isTranslateEnabled) ? translaterService?.translate(labelKey || ' ') : labelKey;
         let prefixText = option[labelPrefixName] || '';
@@ -86,18 +81,18 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
         // add to a temp array for joining purpose and filter out empty text
         const tmpOptionArray = [prefixText, (typeof labelText === 'string' || typeof labelText === 'number') ? labelText.toString() : labelText, suffixText].filter((text) => text);
         let optionText = tmpOptionArray.join(separatorBetweenLabels);
+        const selectOption: OptionRowData = { text: '', value: '' };
 
-        // if user specifically wants to render html text, he needs to opt-in else it will stripped out by default
+        // if user specifically wants to render html text, he needs to opt-in else it will be stripped out by default
         // also, the 3rd party lib will saninitze any html code unless it's encoded, so we'll do that
         if (isRenderHtmlEnabled) {
           // sanitize any unauthorized html tags like script and others
           // for the remaining allowed tags we'll permit all attributes
-          const sanitizedText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
-          optionText = htmlEncode(sanitizedText);
-          selectOptionElm.innerHTML = optionText;
+          optionText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
         } else {
-          selectOptionElm.textContent = optionText;
+          optionText = htmlEncode(optionText);
         }
+        selectOption.text = optionText;
 
         // html text of each select option
         let selectOptionValue = option[valueName];
@@ -106,25 +101,21 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
         }
 
         if (type === 'filter' && Array.isArray(searchTerms)) {
-          selectOptionElm.selected = (searchTerms.findIndex(term => `${term}` === `${option[valueName]}`) >= 0); // when filter search term is found then select it in dropdown
+          selectOption.selected = (searchTerms.findIndex(term => `${term}` === `${option[valueName]}`) >= 0); // when filter search term is found then select it in dropdown
         }
-        selectOptionElm.value = `${selectOptionValue}`;
-        selectOptionElm.label = `${selectOptionLabel ?? ''}`;
-        selectOptionsFragment.appendChild(selectOptionElm);
+        selectOption.value = `${selectOptionValue ?? ''}`; // we'll convert every value to string for better equality checks
+        dataCollection.push(selectOption);
 
         // if there's a search term, we will add the "filled" class for styling purposes
         // on a single select, we'll also make sure the single value is not an empty string to consider this being filled
-        if ((selectOptionElm.selected && isMultiSelect) || (selectOptionElm.selected && !isMultiSelect && option[valueName] !== '')) {
+        if ((selectOption.selected && isMultiSelect) || (selectOption.selected && !isMultiSelect && option[valueName] !== '')) {
           hasFoundSearchTerm = true;
         }
       });
     }
   }
 
-  // last step append the HTML fragment to the final select DOM element
-  selectElement.appendChild(selectOptionsFragment);
-
-  return { selectElement, hasFoundSearchTerm };
+  return { selectElement, dataCollection, hasFoundSearchTerm };
 }
 
 /** calculate available space for each side of the DOM element */
@@ -254,6 +245,24 @@ export function getHtmlElementOffset(element?: HTMLElement): HtmlElementPosition
   return { top, left, bottom, right };
 }
 
+export function getInnerSize(elm: HTMLElement, type: 'height' | 'width') {
+  let size = 0;
+
+  if (elm) {
+    const clientSize = type === 'height' ? 'clientHeight' : 'clientWidth';
+    const sides = type === 'height' ? ['top', 'bottom'] : ['left', 'right'];
+    size = elm[clientSize];
+    for (const side of sides) {
+      size -= (parseFloat(getElementProp(elm, `padding-${side}`)) || 0);
+    }
+  }
+  return size;
+}
+
+export function getElementProp(elm: HTMLElement, property: string) {
+  return window.getComputedStyle(elm, null).getPropertyValue(property);
+}
+
 export function getSelectorStringFromElement(elm?: HTMLElement | null) {
   let selector = '';
   if (elm?.localName) {
@@ -285,8 +294,8 @@ export function findWidthOrDefault(inputWidth?: number | string, defaultValue = 
 }
 
 /**
- * HTML encode using jQuery with a <div>
- * Create a in-memory div, set it's inner text(which jQuery automatically encodes)
+ * HTML encode using a plain <div>
+ * Create a in-memory div, set it's inner text(which a div can encode)
  * then grab the encoded contents back out.  The div never exists on the page.
  */
 export function htmlEncode(inputValue: string): string {

commit 40e1a7c74d6c1b8e41bc7bcc761e0de84569750f

fix(common): Select Filter/Editor regular text shouldn't be html encoded - fix a regression bug introduced by PR #976 when migrating from ms-select to ms-select-vanilla, the text should be encode only when `renderHtmlEnabled` is enabled and not encoded when it's the flag is disabled, basically the html encode was set on the wrong condition

packages/common/src/services/domUtilities.ts
@@ -88,9 +88,7 @@ export function buildMultipleSelectDataCollection(type: 'editor' | 'filter', col
         if (isRenderHtmlEnabled) {
           // sanitize any unauthorized html tags like script and others
           // for the remaining allowed tags we'll permit all attributes
-          optionText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
-        } else {
-          optionText = htmlEncode(optionText);
+          optionText = htmlEncode(sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions));
         }
         selectOption.text = optionText;
 