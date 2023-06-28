ghiscoding/slickgrid-universal

commit 4e3e1d394247be75d1717feece833e200fce21dc

feat!: drop jQuery requirement & use multiple-select-vanilla dependency (#976)
* feat(common)!: migrate to multiple-select-vanilla (#919)

* feat(common)!: migrate to multiple-select-vanilla
- switch from `multiple-select-modified` to [multiple-select-vanilla](https://github.com/ghiscoding/multiple-select-vanilla) which no longer requires jQuery
- might release as a breaking change version since there is a very small change for the CSS import of the new lib, the MultipleSelectOption also no longer exist in Slickgrid-Universal but is instead in the new lib

packages/common/src/filters/selectFilter.ts
@@ -1,3 +1,6 @@
+import { multipleSelect, MultipleSelectInstance, MultipleSelectOption, OptionRowData } from 'multiple-select-vanilla';
+import { isPrimitiveValue } from '@slickgrid-universal/utils';
+
 import { Constants } from '../constants';
 import { type OperatorString, OperatorType, type SearchTerm, } from '../enums/index';
 import type {
@@ -10,35 +13,35 @@ import type {
   FilterCallback,
   GridOption,
   Locale,
-  MultipleSelectOption,
   SlickGrid,
 } from './../interfaces/index';
 import type { CollectionService } from '../services/collection.service';
 import { collectionObserver, propertyObserver } from '../services/observers';
 import { getDescendantProperty, getTranslationPrefix, unsubscribeAll } from '../services/utilities';
-import { buildSelectEditorOrFilterDomElement, type RxJsFacade, type Subscription, type TranslaterService } from '../services/index';
+import { buildMultipleSelectDataCollection, emptyElement, type RxJsFacade, sanitizeTextByAvailableSanitizer, type Subscription, type TranslaterService } from '../services/index';
 import { renderCollectionOptionsAsync } from './filterUtilities';
 
 export class SelectFilter implements Filter {
   protected _isMultipleSelect = true;
   protected _collectionLength = 0;
   protected _locales!: Locale;
+  protected _msInstance?: MultipleSelectInstance;
   protected _shouldTriggerQuery = true;
 
   /** DOM Element Name, useful for auto-detecting positioning (dropup / dropdown) */
   elementName!: string;
 
   /** Filter Multiple-Select options */
-  filterElmOptions!: MultipleSelectOption;
+  filterElmOptions!: Partial<MultipleSelectOption>;
 
-  /** The JQuery DOM element */
-  $filterElm: any;
+  /** The DOM element */
+  filterElm?: HTMLElement;
 
   grid!: SlickGrid;
   searchTerms: SearchTerm[] | undefined;
   columnDef!: Column;
   callback!: FilterCallback;
-  defaultOptions!: MultipleSelectOption;
+  defaultOptions!: Partial<MultipleSelectOption>;
   isFilled = false;
   labelName!: string;
   labelPrefixName!: string;
@@ -90,6 +93,10 @@ export class SelectFilter implements Filter {
     return this._isMultipleSelect;
   }
 
+  get msInstance() {
+    return this._msInstance;
+  }
+
   /** Getter for the Filter Operator */
   get operator(): OperatorType | OperatorString {
     return this.columnFilter?.operator ?? this.defaultOperator;
@@ -102,9 +109,7 @@ export class SelectFilter implements Filter {
     }
   }
 
-  /**
-   * Initialize the filter template
-   */
+  /** Initialize the filter template */
   init(args: FilterArguments): Promise<any[]> {
     if (!args) {
       throw new Error('[Slickgrid-Universal] A filter must always have an "init()" with valid arguments.');
@@ -121,11 +126,11 @@ export class SelectFilter implements Filter {
     }
 
     this.enableTranslateLabel = this.columnFilter?.enableTranslateLabel ?? false;
-    this.labelName = this.customStructure && this.customStructure.label || 'label';
-    this.labelPrefixName = this.customStructure && this.customStructure.labelPrefix || 'labelPrefix';
-    this.labelSuffixName = this.customStructure && this.customStructure.labelSuffix || 'labelSuffix';
-    this.optionLabel = this.customStructure && this.customStructure.optionLabel || 'value';
-    this.valueName = this.customStructure && this.customStructure.value || 'value';
+    this.labelName = this.customStructure?.label ?? 'label';
+    this.labelPrefixName = this.customStructure?.labelPrefix ?? 'labelPrefix';
+    this.labelSuffixName = this.customStructure?.labelSuffix ?? 'labelSuffix';
+    this.optionLabel = this.customStructure?.optionLabel ?? 'value';
+    this.valueName = this.customStructure?.value ?? 'value';
 
     if (this.enableTranslateLabel && (!this.translaterService || typeof this.translaterService.translate !== 'function')) {
       throw new Error(`[select-filter] The Translate Service is required for the Select Filter to work correctly when "enableTranslateLabel" is set.`);
@@ -138,8 +143,8 @@ export class SelectFilter implements Filter {
     this.initMultipleSelectTemplate();
 
     // add placeholder when found
-    let placeholder = this.gridOptions && this.gridOptions.defaultFilterPlaceholder || '';
-    if (this.columnFilter && this.columnFilter.placeholder) {
+    let placeholder = this.gridOptions?.defaultFilterPlaceholder || '';
+    if (this.columnFilter?.placeholder) {
       placeholder = this.columnFilter.placeholder;
     }
     this.defaultOptions.placeholder = placeholder || '';
@@ -183,14 +188,13 @@ export class SelectFilter implements Filter {
     });
   }
 
-  /**
-   * Clear the filter values
-   */
+  /** Clear the filter values */
   clear(shouldTriggerQuery = true) {
-    if (this.$filterElm && this.$filterElm.multipleSelect && this._collectionLength > 0) {
+    if (this._msInstance && this._collectionLength > 0) {
       // reload the filter element by it's id, to make sure it's still a valid element (because of some issue in the GraphQL example)
-      this.$filterElm.multipleSelect('setSelects', []);
-      this.$filterElm.removeClass('filled').siblings('div .search-filter').removeClass('filled');
+      this._msInstance.setSelects([]);
+      this.filterElm?.classList.remove('filled');
+      this._msInstance?.getParentElement()?.classList.remove('filled');
       this.searchTerms = [];
       this._shouldTriggerQuery = shouldTriggerQuery;
       this.callback(undefined, { columnDef: this.columnDef, clearFilterTriggered: true, shouldTriggerQuery: this._shouldTriggerQuery });
@@ -198,17 +202,10 @@ export class SelectFilter implements Filter {
     }
   }
 
-  /**
-   * destroy the filter
-   */
+  /** destroy the filter */
   destroy() {
-    if (this.$filterElm && typeof this.$filterElm.multipleSelect === 'function') {
-      this.$filterElm.multipleSelect('destroy');
-      this.$filterElm.remove();
-      const elementClassName = this.elementName.toString().replace('.', '\\.'); // make sure to escape any dot "." from CSS class to avoid console error
-      $(`[name=${elementClassName}].ms-drop`).remove();
-    }
-    this.$filterElm = null;
+    this._msInstance?.destroy();
+    this.filterElm?.remove();
 
     // unsubscribe all the possible Observables if RxJS was used
     unsubscribeAll(this.subscriptions);
@@ -219,19 +216,19 @@ export class SelectFilter implements Filter {
    * @params selected items
    */
   getValues(): any[] {
-    if (this.$filterElm && typeof this.$filterElm.multipleSelect === 'function') {
-      return this.$filterElm.multipleSelect('getSelects');
-    }
-    return [];
+    return this._msInstance?.getSelects() ?? [];
   }
 
   /** Set value(s) on the DOM element */
   setValues(values: SearchTerm | SearchTerm[], operator?: OperatorType | OperatorString) {
-    if (values && this.$filterElm && typeof this.$filterElm.multipleSelect === 'function') {
-      values = Array.isArray(values) ? values : [values];
-      this.$filterElm.multipleSelect('setSelects', values);
+    if (values !== undefined && this._msInstance) {
+      values = Array.isArray(values)
+        ? values.every(x => isPrimitiveValue(x)) ? values.map(String) : values
+        : [values];
+      this._msInstance.setSelects(values);
     }
     this.updateFilterStyle(this.getValues().length > 0);
+
     // set the operator when defined
     this.operator = operator || this.defaultOperator;
   }
@@ -251,7 +248,7 @@ export class SelectFilter implements Filter {
     // user might want to filter certain items of the collection
     if (this.columnFilter && this.columnFilter.collectionFilterBy) {
       const filterBy = this.columnFilter.collectionFilterBy;
-      const filterCollectionBy = this.columnFilter.collectionOptions && this.columnFilter.collectionOptions.filterResultAfterEachPass || null;
+      const filterCollectionBy = this.columnFilter.collectionOptions?.filterResultAfterEachPass || null;
       outputCollection = this.collectionService.filterCollection(outputCollection, filterBy, filterCollectionBy);
     }
 
@@ -344,7 +341,7 @@ export class SelectFilter implements Filter {
     newCollection = this.sortCollection(newCollection);
 
     // step 1, create HTML DOM element
-    const selectBuildResult = buildSelectEditorOrFilterDomElement(
+    const selectBuildResult = buildMultipleSelectDataCollection(
       'filter',
       newCollection,
       this.columnDef,
@@ -357,7 +354,7 @@ export class SelectFilter implements Filter {
 
     // step 2, create the DOM Element of the filter & pre-load search terms
     // we will later also subscribe to the onClose event to filter the data whenever that event is triggered
-    this.createFilterElement(selectBuildResult.selectElement);
+    this.createFilterElement(selectBuildResult.selectElement, selectBuildResult.dataCollection);
     this._collectionLength = newCollection.length;
   }
 
@@ -377,77 +374,70 @@ export class SelectFilter implements Filter {
   }
 
   /**
-   * From the Select DOM Element created earlier, create a Multiple/Single Select Filter using the jQuery multiple-select.js lib
+   * From the Select DOM Element created earlier, create a Multiple/Single Select Filter using the multiple-select-vanilla.js lib
    * @param {Object} selectElement
    */
-  protected createFilterElement(selectElement: HTMLSelectElement) {
+  protected createFilterElement(selectElement: HTMLSelectElement, dataCollection: OptionRowData[]) {
     const columnId = this.columnDef?.id ?? '';
 
     // provide the name attribute to the DOM element which will be needed to auto-adjust drop position (dropup / dropdown)
     this.elementName = `filter-${columnId}`;
     this.defaultOptions.name = this.elementName;
 
-    $(this.filterContainerElm).empty();
+    emptyElement(this.filterContainerElm);
 
     // create the DOM element & add an ID and filter class
-    this.$filterElm = $(selectElement);
-    if (typeof this.$filterElm.multipleSelect !== 'function') {
-      throw new Error(`multiple-select.js was not found, make sure to read the HOWTO Wiki on how to install it.`);
-    }
-    this.$filterElm.data('columnId', columnId);
+    this.filterElm = selectElement;
+    this.filterElm.dataset.columnId = `${columnId}`;
 
     // if there's a search term, we will add the "filled" class for styling purposes
     this.updateFilterStyle(this.isFilled);
 
     // append the new DOM element to the header row
-    if (this.$filterElm && typeof this.$filterElm.appendTo === 'function') {
-      this.$filterElm.appendTo(this.filterContainerElm);
-    }
+    this.filterContainerElm.appendChild(selectElement);
 
     // merge options & attach multiSelect
     const filterOptions: MultipleSelectOption = (this.columnFilter) ? this.columnFilter.filterOptions : {};
-    this.filterElmOptions = { ...this.defaultOptions, ...(filterOptions as MultipleSelectOption) };
-    this.$filterElm = this.$filterElm.multipleSelect(this.filterElmOptions);
+    this.filterElmOptions = { ...this.defaultOptions, ...(filterOptions as MultipleSelectOption), data: dataCollection };
+    this._msInstance = multipleSelect(selectElement, this.filterElmOptions) as MultipleSelectInstance;
   }
 
   protected initMultipleSelectTemplate() {
     const isTranslateEnabled = this.gridOptions?.enableTranslate ?? false;
+    const columnId = this.columnDef?.id ?? '';
 
     // default options used by this Filter, user can overwrite any of these by passing "otions"
-    const options: MultipleSelectOption = {
+    const options = {
       autoAdjustDropHeight: true,
       autoAdjustDropPosition: true,
       autoAdjustDropWidthByTextSize: true,
+      name: `${columnId}`,
       container: 'body',
       filter: false,  // input search term on top of the select option list
       maxHeight: 275,
       single: true,
-      textTemplate: ($elm) => {
-        // are we rendering HTML code? by default it is sanitized and won't be rendered
-        const isRenderHtmlEnabled = this.columnDef && this.columnDef.filter && this.columnDef.filter.enableRenderHtml || false;
-        return isRenderHtmlEnabled ? $elm.text() : $elm.html();
-      },
+      sanitizer: (dirtyHtml: string) => sanitizeTextByAvailableSanitizer(this.gridOptions, dirtyHtml),
       // we will subscribe to the onClose event for triggering our callback
       // also add/remove "filled" class for styling purposes
       onClose: () => this.onTriggerEvent()
-    };
+    } as MultipleSelectOption;
 
     if (this._isMultipleSelect) {
       options.single = false;
-      options.okButton = true;
-      options.addTitle = true; // show tooltip of all selected items while hovering the filter
+      options.showOkButton = true;
+      options.displayTitle = true; // show tooltip of all selected items while hovering the filter
       const translationPrefix = getTranslationPrefix(this.gridOptions);
-      options.countSelected = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}X_OF_Y_SELECTED`) : this._locales?.TEXT_X_OF_Y_SELECTED;
-      options.allSelected = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}ALL_SELECTED`) : this._locales?.TEXT_ALL_SELECTED;
+      options.countSelectedText = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}X_OF_Y_SELECTED`) : this._locales?.TEXT_X_OF_Y_SELECTED;
+      options.allSelectedText = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}ALL_SELECTED`) : this._locales?.TEXT_ALL_SELECTED;
+      options.noMatchesFoundText = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}NO_MATCHES_FOUND`) : this._locales?.TEXT_NO_MATCHES_FOUND;
       options.okButtonText = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}OK`) : this._locales?.TEXT_OK;
       options.selectAllText = (isTranslateEnabled && this.translaterService?.translate) ? this.translaterService.translate(`${translationPrefix}SELECT_ALL`) : this._locales?.TEXT_SELECT_ALL;
-      options.selectAllDelimiter = ['', '']; // remove default square brackets of default text "[Select All]" => "Select All"
     }
     this.defaultOptions = options;
   }
 
   protected onTriggerEvent() {
-    if (this.$filterElm) {
+    if (this._msInstance) {
       const selectedItems = this.getValues();
       this.updateFilterStyle(Array.isArray(selectedItems) && selectedItems.length > 1 || (selectedItems.length === 1 && selectedItems[0] !== ''));
       this.searchTerms = selectedItems;
@@ -461,11 +451,12 @@ export class SelectFilter implements Filter {
   protected updateFilterStyle(isFilled: boolean) {
     if (isFilled) {
       this.isFilled = true;
-      this.$filterElm?.addClass('filled').siblings('div .search-filter').addClass('filled');
+      this.filterElm?.classList.add('filled');
+      this._msInstance?.getParentElement()?.classList.add('filled');
     } else {
       this.isFilled = false;
-      this.$filterElm.removeClass('filled');
-      this.$filterElm.siblings('div .search-filter').removeClass('filled');
+      this.filterElm?.classList.remove('filled');
+      this._msInstance?.getParentElement()?.classList.remove('filled');
     }
   }
 }

packages/common/src/services/domUtilities.ts
@@ -1,3 +1,4 @@
+import { OptionRowData } from 'multiple-select-vanilla';
 import * as DOMPurify_ from 'dompurify';
 const DOMPurify = ((DOMPurify_ as any)?.['default'] ?? DOMPurify_); // patch for rollup
 
@@ -16,7 +17,7 @@ import type { TranslaterService } from './translater.service';
  * @param {Array<*>} searchTerms - optional array of search term (used by the "filter" type only)
  * @returns object with 2 properties for the select element & a boolean value telling us if any of the search terms were found and selected in the dropdown
  */
-export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', collection: any[], columnDef: Column, grid: SlickGrid, isMultiSelect = false, translaterService?: TranslaterService, searchTerms?: SearchTerm[]): { selectElement: HTMLSelectElement; hasFoundSearchTerm: boolean; } {
+export function buildMultipleSelectDataCollection(type: 'editor' | 'filter', collection: any[], columnDef: Column, grid: SlickGrid, isMultiSelect = false, translaterService?: TranslaterService, searchTerms?: SearchTerm[]): { selectElement: HTMLSelectElement; dataCollection: OptionRowData[]; hasFoundSearchTerm: boolean; } {
   const columnId = columnDef?.id ?? '';
   const gridOptions = grid.getOptions();
   const columnFilterOrEditor = (type === 'editor' ? columnDef?.internalColumnEditor : columnDef?.filter) ?? {};
@@ -37,38 +38,32 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
   selectElement.classList.add(...extraCssClasses);
 
   selectElement.multiple = isMultiSelect;
-
-  // use an HTML Fragment for performance reason, MDN explains it well as shown below::
-  // The key difference is that because the document fragment isn't part of the actual DOM's structure, changes made to the fragment don't affect the document, cause reflow, or incur any performance impact that can occur when changes are made.
-  const selectOptionsFragment = document.createDocumentFragment();
-
+  const dataCollection: OptionRowData[] = [];
   let hasFoundSearchTerm = false;
 
   // collection could be an Array of Strings OR Objects
   if (Array.isArray(collection)) {
-    if (collection.every((x: any) => typeof x === 'string')) {
+    if (collection.every((x: any) => typeof x === 'number' || typeof x === 'string')) {
       for (const option of collection) {
-        const selectOptionElm = createDomElement('option', {
-          label: option, value: option, textContent: option,
-        });
+        const selectOption: OptionRowData = { text: option, value: option };
         if (type === 'filter' && Array.isArray(searchTerms)) {
-          selectOptionElm.selected = (searchTerms.findIndex(term => term === option) >= 0); // when filter search term is found then select it in dropdown
+          selectOption.selected = (searchTerms.findIndex(term => term === option) >= 0); // when filter search term is found then select it in dropdown
         }
-        selectOptionsFragment.appendChild(selectOptionElm);
+        dataCollection.push(selectOption);
 
         // if there's at least 1 Filter search term found, we will add the "filled" class for styling purposes
         // on a single select, we'll also make sure the single value is not an empty string to consider this being filled
-        if ((selectOptionElm.selected && isMultiSelect) || (selectOptionElm.selected && !isMultiSelect && option !== '')) {
+        if ((selectOption.selected && isMultiSelect) || (selectOption.selected && !isMultiSelect && option !== '')) {
           hasFoundSearchTerm = true;
         }
       }
     } else {
       // array of objects will require a label/value pair unless a customStructure is passed
       collection.forEach((option: SelectOption) => {
-        if (!option || (option[labelName] === undefined && option.labelKey === undefined)) {
+        if (option === undefined || (typeof option === 'object' && option[labelName] === undefined && option.labelKey === undefined)) {
           throw new Error(`[Slickgrid-Universal] Select Filter/Editor collection with value/label (or value/labelKey when using Locale) is required to populate the Select list, for example:: { filter: model: Filters.multipleSelect, collection: [ { value: '1', label: 'One' } ]')`);
         }
-        const selectOptionElm = document.createElement('option');
+
         const labelKey = (option.labelKey || option[labelName]) as string;
         const labelText = ((option.labelKey || (enableTranslateLabel && translaterService)) && labelKey && isTranslateEnabled) ? translaterService?.translate(labelKey || ' ') : labelKey;
         let prefixText = option[labelPrefixName] || '';
@@ -86,18 +81,18 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
         // add to a temp array for joining purpose and filter out empty text
         const tmpOptionArray = [prefixText, (typeof labelText === 'string' || typeof labelText === 'number') ? labelText.toString() : labelText, suffixText].filter((text) => text);
         let optionText = tmpOptionArray.join(separatorBetweenLabels);
+        const selectOption: OptionRowData = { text: '', value: '' };
 
-        // if user specifically wants to render html text, he needs to opt-in else it will stripped out by default
+        // if user specifically wants to render html text, he needs to opt-in else it will be stripped out by default
         // also, the 3rd party lib will saninitze any html code unless it's encoded, so we'll do that
         if (isRenderHtmlEnabled) {
           // sanitize any unauthorized html tags like script and others
           // for the remaining allowed tags we'll permit all attributes
-          const sanitizedText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
-          optionText = htmlEncode(sanitizedText);
-          selectOptionElm.innerHTML = optionText;
+          optionText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
         } else {
-          selectOptionElm.textContent = optionText;
+          optionText = htmlEncode(optionText);
         }
+        selectOption.text = optionText;
 
         // html text of each select option
         let selectOptionValue = option[valueName];
@@ -106,25 +101,21 @@ export function buildSelectEditorOrFilterDomElement(type: 'editor' | 'filter', c
         }
 
         if (type === 'filter' && Array.isArray(searchTerms)) {
-          selectOptionElm.selected = (searchTerms.findIndex(term => `${term}` === `${option[valueName]}`) >= 0); // when filter search term is found then select it in dropdown
+          selectOption.selected = (searchTerms.findIndex(term => `${term}` === `${option[valueName]}`) >= 0); // when filter search term is found then select it in dropdown
         }
-        selectOptionElm.value = `${selectOptionValue}`;
-        selectOptionElm.label = `${selectOptionLabel ?? ''}`;
-        selectOptionsFragment.appendChild(selectOptionElm);
+        selectOption.value = `${selectOptionValue ?? ''}`; // we'll convert every value to string for better equality checks
+        dataCollection.push(selectOption);
 
         // if there's a search term, we will add the "filled" class for styling purposes
         // on a single select, we'll also make sure the single value is not an empty string to consider this being filled
-        if ((selectOptionElm.selected && isMultiSelect) || (selectOptionElm.selected && !isMultiSelect && option[valueName] !== '')) {
+        if ((selectOption.selected && isMultiSelect) || (selectOption.selected && !isMultiSelect && option[valueName] !== '')) {
           hasFoundSearchTerm = true;
         }
       });
     }
   }
 
-  // last step append the HTML fragment to the final select DOM element
-  selectElement.appendChild(selectOptionsFragment);
-
-  return { selectElement, hasFoundSearchTerm };
+  return { selectElement, dataCollection, hasFoundSearchTerm };
 }
 
 /** calculate available space for each side of the DOM element */
@@ -254,6 +245,24 @@ export function getHtmlElementOffset(element?: HTMLElement): HtmlElementPosition
   return { top, left, bottom, right };
 }
 
+export function getInnerSize(elm: HTMLElement, type: 'height' | 'width') {
+  let size = 0;
+
+  if (elm) {
+    const clientSize = type === 'height' ? 'clientHeight' : 'clientWidth';
+    const sides = type === 'height' ? ['top', 'bottom'] : ['left', 'right'];
+    size = elm[clientSize];
+    for (const side of sides) {
+      size -= (parseFloat(getElementProp(elm, `padding-${side}`)) || 0);
+    }
+  }
+  return size;
+}
+
+export function getElementProp(elm: HTMLElement, property: string) {
+  return window.getComputedStyle(elm, null).getPropertyValue(property);
+}
+
 export function getSelectorStringFromElement(elm?: HTMLElement | null) {
   let selector = '';
   if (elm?.localName) {
@@ -285,8 +294,8 @@ export function findWidthOrDefault(inputWidth?: number | string, defaultValue = 
 }
 
 /**
- * HTML encode using jQuery with a <div>
- * Create a in-memory div, set it's inner text(which jQuery automatically encodes)
+ * HTML encode using a plain <div>
+ * Create a in-memory div, set it's inner text(which a div can encode)
  * then grab the encoded contents back out.  The div never exists on the page.
  */
 export function htmlEncode(inputValue: string): string {

commit c203a2ce4d4e5cf6dfb0e05a25f5fd6b0c4cbe4d

fix(common): Select Filter/Editor regular text shouldn't be html encoded (#1011)
* fix(common): Select Filter/Editor regular text shouldn't be html encoded
- fix a regression bug introduced by PR #976 when migrating from ms-select to ms-select-vanilla, the text should be encode only when `renderHtmlEnabled` is enabled and not encoded when it's the flag is disabled, basically the html encode was set on the wrong condition

packages/common/src/filters/selectFilter.ts
@@ -416,6 +416,7 @@ export class SelectFilter implements Filter {
       filter: false,  // input search term on top of the select option list
       maxHeight: 275,
       single: true,
+      useSelectOptionLabelToHtml: this.columnFilter?.enableRenderHtml ?? false,
       sanitizer: (dirtyHtml: string) => sanitizeTextByAvailableSanitizer(this.gridOptions, dirtyHtml),
       // we will subscribe to the onClose event for triggering our callback
       // also add/remove "filled" class for styling purposes

packages/common/src/services/domUtilities.ts
@@ -89,8 +89,6 @@ export function buildMultipleSelectDataCollection(type: 'editor' | 'filter', col
           // sanitize any unauthorized html tags like script and others
           // for the remaining allowed tags we'll permit all attributes
           optionText = sanitizeTextByAvailableSanitizer(gridOptions, optionText, sanitizedOptions);
-        } else {
-          optionText = htmlEncode(optionText);
         }
         selectOption.text = optionText;
 
@@ -297,16 +295,19 @@ export function findWidthOrDefault(inputWidth?: number | string, defaultValue = 
  * HTML encode using a plain <div>
  * Create a in-memory div, set it's inner text(which a div can encode)
  * then grab the encoded contents back out.  The div never exists on the page.
+ * @param {String} inputValue - input value to be encoded
+ * @return {String}
  */
 export function htmlEncode(inputValue: string): string {
-  const entityMap = {
+  const val = typeof inputValue === 'string' ? inputValue : String(inputValue);
+  const entityMap: { [char: string]: string; } = {
     '&': '&amp;',
     '<': '&lt;',
     '>': '&gt;',
     '"': '&quot;',
-    '\'': '&#39;'
+    '\'': '&#39;',
   };
-  return (inputValue || '').toString().replace(/[&<>"']/g, (s) => (entityMap as any)[s]);
+  return (val || '').toString().replace(/[&<>"']/g, (s) => entityMap[s as keyof { [char: string]: string; }]);
 }
 
 /**

pnpm-lock.yaml
@@ -173,8 +173,8 @@ importers:
         specifier: ^2.29.4
         version: 2.29.4
       multiple-select-vanilla:
-        specifier: ^0.4.3
-        version: 0.4.3
+        specifier: ^0.4.4
+        version: 0.4.4
       rxjs:
         specifier: ^7.8.1
         version: 7.8.1
@@ -243,8 +243,8 @@ importers:
         specifier: ^2.29.4
         version: 2.29.4
       multiple-select-vanilla:
-        specifier: ^0.4.3
-        version: 0.4.3
+        specifier: ^0.4.4
+        version: 0.4.4
       slickgrid:
         specifier: ^4.0.0
         version: 4.0.0
@@ -7079,8 +7079,8 @@ packages:
       minimatch: 3.1.2
     dev: true
 
-  /multiple-select-vanilla@0.4.3:
-    resolution: {integrity: sha512-l3MNcn/eB1TZKPUlGuNFEKix16vbKCkeksHFUeNCWHSZsgTo0GezpHiT82BSwtDLzjwx1Q4ykKvXKG/49Jk8Wg==}
+  /multiple-select-vanilla@0.4.4:
+    resolution: {integrity: sha512-R6Sl93PyYAGL8LFUkp8W+ojdBEdVNZYd/eV2H+37rcolMKgyssjA5KsHO1d+5JnO6rr4Xm7npAmGfuHNehBM0g==}
     dev: false
 
   /mute-stream@1.0.0: