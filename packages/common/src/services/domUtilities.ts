98c621aa87bc3fd1e505969a02675ff3b2c53654
ghiscoding <gbeaulac@gmail.com>
2023-06-28 15:42:56 -0400

    chore: update to latest ms-select-vanilla with fix to not render html by default - also fix an issue with `htmlEncode` that would return empty string when input was the number 0

--- packages/common/src/services/domUtilities.ts
+++ packages/common/src/services/domUtilities.ts
@@ -295,16 +295,19 @@ export function findWidthOrDefault(inputWidth?: number | string, defaultValue = 
  * HTML encode using a plain <div>
  * Create a in-memory div, set it's inner text(which a div can encode)
  * then grab the encoded contents back out.  The div never exists on the page.
+ * @param {String} inputValue - input value to be encoded
+ * @return {String}
  */
 export function htmlEncode(inputValue: string): string {
-  const entityMap = {
+  const val = typeof inputValue === 'string' ? inputValue : String(inputValue);
+  const entityMap: { [char: string]: string; } = {
     '&': '&amp;',
     '<': '&lt;',
     '>': '&gt;',
     '"': '&quot;',
-    '\'': '&#39;'
+    '\'': '&#39;',
   };
-  return (inputValue || '').toString().replace(/[&<>"']/g, (s) => (entityMap as any)[s]);
+  return (val || '').toString().replace(/[&<>"']/g, (s) => entityMap[s as keyof { [char: string]: string; }]);
 }
 
 /**
