a1a1fe531f1e8c8355ff06bc33c1f859ddc5b4ed
ghiscoding <gbeaulac@gmail.com>
2023-10-04 21:57:14 -0400

    chore: improve Example 19 & add Cypress E2E tests suite

--- examples/vite-demo-vanilla-bundle/src/examples/example19.ts
+++ examples/vite-demo-vanilla-bundle/src/examples/example19.ts
@@ -1,17 +1,13 @@
-import {
-  Column,
-  FieldType,
-  Filters,
-  Formatters,
-  GridOption,
-} from '@slickgrid-universal/common';
+import { CellRange, Column, GridOption, SlickEventHandler, SlickNamespace, } from '@slickgrid-universal/common';
 import { Slicker, SlickVanillaGridBundle } from '@slickgrid-universal/vanilla-bundle';
 import { ExampleGridOptions } from './example-grid-options';
-import '../material-styles.scss';
+import '../salesforce-styles.scss';
+import './example19.scss';
 
-const NB_ITEMS = 500;
-
+const NB_ITEMS = 100;
+declare const Slick: SlickNamespace;
 export default class Example34 {
+  protected _eventHandler: SlickEventHandler;
   title = 'Example 19: ExcelCopyBuffer with Cell Selection';
   subTitle = `Cell Selection using "Shift+{key}" where "key" can be any of:
   <ul>
@@ -24,75 +20,76 @@ export default class Example34 {
   columnDefinitions: Column[] = [];
   dataset: any[] = [];
   gridOptions!: GridOption;
+  gridContainerElm: HTMLDivElement;
   isWithPagination = true;
   sgb: SlickVanillaGridBundle;
 
   attached() {
+    this._eventHandler = new Slick.EventHandler();
+
     // define the grid options & columns and then create the grid itself
     this.defineGrid();
 
     // mock some data (different in each dataset)
     this.dataset = this.getData(NB_ITEMS);
+    this.gridContainerElm = document.querySelector<HTMLDivElement>(`.grid19`) as HTMLDivElement;
     this.sgb = new Slicker.GridBundle(document.querySelector(`.grid19`) as HTMLDivElement, this.columnDefinitions, { ...ExampleGridOptions, ...this.gridOptions }, this.dataset);
-    document.body.classList.add('material-theme');
+    document.body.classList.add('salesforce-theme');
+
+    // bind any of the grid events
+    const cellSelectionModel = this.sgb.slickGrid!.getSelectionModel();
+    this._eventHandler.subscribe(cellSelectionModel!.onSelectedRangesChanged, (_e, args: CellRange[]) => {
+      const targetRange = document.querySelector('#selectionRange') as HTMLSpanElement;
+      targetRange.textContent = '';
+
+      for (const slickRange of args) {
+        targetRange.textContent += JSON.stringify(slickRange);
+      }
+    });
   }
 
   dispose() {
+    this._eventHandler.unsubscribeAll();
     this.sgb?.dispose();
-    document.body.classList.remove('material-theme');
+    this.gridContainerElm.remove();
+    document.body.classList.remove('salesforce-theme');
   }
 
   /* Define grid Options and Columns */
   defineGrid() {
-    // the columns field property is type-safe, try to add a different string not representing one of DataItems properties
     this.columnDefinitions = [
-      { id: 'title', name: 'Title', field: 'title', sortable: true, type: FieldType.string, width: 70, filterable: true },
-      { id: 'phone', name: 'Phone Number using mask', field: 'phone', sortable: true, type: FieldType.number, minWidth: 100, formatter: Formatters.mask, params: { mask: '(000) 000-0000' }, filterable: true },
       {
-        id: 'duration', name: 'Duration (days)', field: 'duration', formatter: Formatters.decimal, params: { minDecimal: 1, maxDecimal: 2 },
-        sortable: true, type: FieldType.number, minWidth: 90, exportWithFormatter: true, filterable: true, filter: { model: Filters.compoundInputNumber }
-      },
-      { id: 'complete', name: '% Complete', field: 'percentComplete', formatter: Formatters.percentCompleteBar, type: FieldType.number, sortable: true, minWidth: 100, filterable: true, filter: { model: Filters.slider } },
-      { id: 'start', name: 'Start', field: 'start', formatter: Formatters.dateIso, sortable: true, type: FieldType.date, minWidth: 90, exportWithFormatter: true, filterable: true },
-      { id: 'finish', name: 'Finish', field: 'finish', formatter: Formatters.dateIso, sortable: true, type: FieldType.date, minWidth: 90, exportWithFormatter: true, filterable: true },
-      {
-        id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', type: FieldType.boolean, sortable: true, minWidth: 100 , filterable: true,
-        formatter: (_row, _cell, value) => {
-          // you can return a string of a object (of type FormatterResultObject), the 2 types are shown below
-          return value ? `<i class="mdi mdi-fire color-danger" aria-hidden="true"></i>` : { text: '<i class="mdi mdi-snowflake color-info-light" aria-hidden="true"></i>', toolTip: 'Freezing' };
-        },
-        filter: {
-          enableRenderHtml: true,
-          collection: [
-            { value: '', label: '' },
-            { value: true, labelPrefix: '<i class="mdi-v-align-middle mdi mdi-fire color-danger" aria-hidden="true"></i> ', label: 'a lot of effort' },
-            { value: false, labelPrefix: '<i class="mdi-v-align-middle mdi mdi-snowflake color-info-light" aria-hidden="true"></i> ', label: 'not that much' }
-          ],
-          model: Filters.singleSelect
-        },
-      },
-      {
-        id: 'completed', name: 'Completed', field: 'completed', type: FieldType.boolean, sortable: true, minWidth: 100, filterable: true,
-        formatter: (_row, _cell, value) => value ? '<i class="mdi mdi-check"></i>' : '',
-        filter: {
-          collection: [{ value: '', label: '' }, { value: true, label: 'True' }, { value: false, label: 'False' }],
-          model: Filters.singleSelect
-        },
+        id: 'selector',
+        name: '',
+        field: 'num',
+        width: 30
       }
     ];
+
+    for (let i = 0; i < NB_ITEMS; i++) {
+      this.columnDefinitions.push({
+        id: i,
+        name: i < 26
+          ? String.fromCharCode('A'.charCodeAt(0) + (i % 26))
+          : String.fromCharCode('A'.charCodeAt(0) + ((i / 26) | 0) -1) + String.fromCharCode('A'.charCodeAt(0) + (i % 26)),
+        field: i as any,
+        minWidth: 60,
+        width: 60,
+      });
+    }
 
     this.gridOptions = {
       autoResize: {
         container: '.demo-container',
       },
       enableCellNavigation: true,
-      enableFiltering: true,
       enablePagination: true,
       pagination: {
         pageSizes: [5, 10, 15, 20, 25, 50, 75, 100],
-        pageSize: 10
+        pageSize: 20
       },
-      rowHeight: 40,
+      headerRowHeight: 35,
+      rowHeight: 30,
 
       // when using the ExcelCopyBuffer, you can see what the selection range is
       enableExcelCopyBuffer: true,
@@ -108,24 +105,11 @@ export default class Example34 {
     // mock a dataset
     const datasetTmp: any[] = [];
     for (let i = 0; i < itemCount; i++) {
-      const randomYear = 2000 + Math.floor(Math.random() * 20);
-      const randomMonth = Math.floor(Math.random() * 11);
-      const randomDay = Math.floor((Math.random() * 29));
-      const randomPercent = Math.round(Math.random() * 100);
-
-      datasetTmp[i] = {
-        id: i,
-        title: 'Task ' + i,
-        phone: this.generatePhoneNumber(),
-        duration: Math.random() * 100 + '',
-        percentComplete: randomPercent,
-        percentCompleteNumber: randomPercent,
-        start: new Date(randomYear, randomMonth, randomDay),
-        finish: new Date(randomYear, (randomMonth + 1), randomDay),
-        effortDriven: (i % 4 === 0),
-        completed: (i % 3 === 0)
-      };
+      const d: any = (datasetTmp[i] = {});
+      d['id'] = i;
+      d['num'] = i;
     }
+
     return datasetTmp;
   }
 
--- test/cypress/e2e/example19.cy.ts
+++ test/cypress/e2e/example19.cy.ts
@@ -1,6 +1,10 @@
-describe('Example 19 - ExcelCopyBuffer with Cell Selection', { retries: 1 }, () => {
-  const titles = ['Title', 'Phone Number using mask', 'Duration (days)', '% Complete', 'Start', 'Finish', 'Effort Driven', 'Completed'];
-  const GRID_ROW_HEIGHT = 40;
+describe('Example 19 - ExcelCopyBuffer with Cell Selection', { retries: 0 }, () => {
+  const titles = [
+    '', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK'
+  ];
+  const GRID_ROW_HEIGHT = 30;
 
   it('should display Example title', () => {
     cy.visit(`${Cypress.config('baseUrl')}/example19`);
@@ -11,15 +15,133 @@ describe('Example 19 - ExcelCopyBuffer with Cell Selection', { retries: 1 }, () 
     cy.get('.grid19')
       .find('.slick-header-columns')
       .children()
-      .each(($child, index) => expect($child.text()).to.eq(titles[index]));
+      .each(($child, index) => {
+        if (index < titles.length) {
+          expect($child.text()).to.eq(titles[index]);
+        }
+      });
   });
 
-  it('should check first 5 rows and expect certain data', () => {
-    for (let i = 0; i < 5; i++) {
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(0)`).contains(`Task ${i}`);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(1)`).contains(/\(\d{3}\)\s\d{3}\-\d{4}/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(2)`).contains(/[0-9\.]*/);
-    }
+  describe('with Pagination of size 20', () => {
+    it('should click on cell B14 then Shift+End w/selection B14-19', () => {
+      cy.getCell(14, 2, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_B14')
+        .click();
+
+      cy.get('@cell_B14')
+        .type('{shift}{end}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":14,"fromCell":2,"toRow":19,"toCell":2}');
+    });
+
+    it('should click on cell C19 then Shift+End w/selection C0-19', () => {
+      cy.getCell(19, 2, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_C19')
+        .click();
+
+      cy.get('@cell_C19')
+        .type('{shift}{home}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":0,"fromCell":2,"toRow":19,"toCell":2}');
+    });
+
+    it('should click on cell E3 then Shift+PageDown multiple times with current page selection starting at E3 w/selection E3-19', () => {
+      cy.getCell(3, 5, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_E3')
+        .click();
+
+      cy.get('@cell_E3')
+        .type('{shift}{pagedown}{pagedown}{pagedown}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":3,"fromCell":5,"toRow":19,"toCell":5}');
+    });
+
+    it('should change to 2nd page then click on cell D41 then Shift+PageUp multiple times with current page selection w/selection D25-41', () => {
+      cy.get('.slick-pagination .icon-seek-next').click();
+
+      cy.getCell(15, 4, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_D41')
+        .click();
+
+      cy.get('@cell_D41')
+        .type('{shift}{pageup}{pageup}{pageup}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":0,"fromCell":4,"toRow":15,"toCell":4}');
+    });
   });
 
+  describe('no Pagination - showing all', () => {
+    it('should hide Pagination', () => {
+      cy.get('[data-text="toggle-pagination-btn"]')
+        .click();
+    });
+
+    it('should click on cell B10 and ArrowUp 3 times and ArrowDown 1 time and expect cell selection B8-B10', () => {
+      cy.getCell(10, 2, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_B10')
+        .click();
+
+      cy.get('@cell_B10')
+        .type('{shift}{uparrow}{uparrow}{uparrow}{downarrow}');
+
+      cy.get('.slick-cell.l2.r2.selected')
+        .should('have.length', 3);
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":8,"fromCell":2,"toRow":10,"toCell":2}');
+    });
+
+    it('should click on cell D10 then PageDown 2 times w/selection D10-D52 ', () => {
+      // 52 is because of a page row count found to be 21 for current browser resolution set in Cypress => 21*2+10 = 52
+      cy.getCell(10, 4, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_D10')
+        .click();
+
+      cy.get('@cell_D10')
+        .type('{shift}{pagedown}{pagedown}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":10,"fromCell":4,"toRow":52,"toCell":4}');
+    });
+
+    it('should click on cell D10 then PageDown 3 times then PageUp 1 time w/selection D10-D52', () => {
+      cy.getCell(10, 4, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_D10')
+        .click();
+
+      cy.get('@cell_D10')
+        .type('{shift}{pagedown}{pagedown}{pagedown}{pageup}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":10,"fromCell":4,"toRow":52,"toCell":4}');
+    });
+
+    it('should click on cell E12 then End key w/selection E52-E99', () => {
+      cy.getCell(52, 5, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_E52')
+        .click();
+
+      cy.get('@cell_E52')
+        .type('{shift}{end}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":52,"fromCell":5,"toRow":99,"toCell":5}');
+    });
+
+    it('should click on cell C85 then End key w/selection C0-C85', () => {
+      cy.getCell(85, 3, '', { parentSelector: '.grid19', rowHeight: GRID_ROW_HEIGHT })
+        .as('cell_C85')
+        .click();
+
+      cy.get('@cell_C85')
+        .type('{shift}{home}');
+
+      cy.get('#selectionRange')
+        .should('have.text', '{"fromRow":0,"fromCell":3,"toRow":85,"toCell":3}');
+    });
+  });
 });
