550f1031ca2c56649ed630ab753d757a3fb799fa
Ghislain B <gbeaulac@gmail.com>
2023-10-31 02:22:22 -0400

    fix(common): mouseover disabled sub-menu shouldn't open it (#1167)
    - the logic should be the same as the sub-menu click event, if the item is disabled (or is a didiver), then we shouldn't go further and not try to open it

--- packages/common/src/extensions/menuBaseClass.ts
+++ packages/common/src/extensions/menuBaseClass.ts
@@ -299,13 +299,6 @@ export class MenuBaseClass<M extends CellMenu | ContextMenu | GridMenu | HeaderM
           undefined,
           eventGroupName
         );
-        // this._bindEventService.bind(commandLiElm, 'mouseover', ((e: DOMMouseOrTouchEvent<HTMLDivElement>) => {
-        //   if ((item as MenuCommandItem).commandItems || (item as MenuOptionItem).optionItems || (item as HeaderMenuCommandItem).items) {
-        //     (this as any).repositionSubMenu(item, itemType, args.level, e);
-        //   } else if (level === 0) {
-        //     this.disposeSubMenus();
-        //   }
-        // }) as EventListener);
       }
 
       // the option/command item could be a sub-menu if it has another list of commands/options
