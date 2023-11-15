443bd390afe9d9ed71a938cccb2ae2dcb7e6aaa3
ghiscoding <gbeaulac@gmail.com>
2023-11-15 01:35:18 -0500

    chore: small commit to trigger CI

--- packages/common/src/interfaces/slickGridModel.interface.ts
+++ packages/common/src/interfaces/slickGridModel.interface.ts
@@ -1,5 +1,5 @@
 
 import type { SlickGrid } from '../core/slick.grid';
-import type { Column, GridOption } from './index';
+import type { Column, GridOption } from './index'; // test
 
 export type SlickGridModel<T = any, C extends Column<T> = Column<T>, O extends GridOption<C> = GridOption<C>> = SlickGrid<T, C, O>;
