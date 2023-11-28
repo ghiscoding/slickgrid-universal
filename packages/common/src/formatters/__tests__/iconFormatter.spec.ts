f94fb1296b15ee65f03c03427d4571e8ad15ebcb
ghiscoding <gbeaulac@gmail.com>
2023-11-27 20:29:10 -0500

    chore: make console spy global to avoid showing warning in unit tests

--- packages/common/src/formatters/__tests__/iconFormatter.spec.ts
+++ packages/common/src/formatters/__tests__/iconFormatter.spec.ts
@@ -2,6 +2,8 @@ import { Column } from '../../interfaces/index';
 import { iconFormatter } from '../iconFormatter';
 
 describe('the Icon Formatter', () => {
+  const consoleWarnSpy = jest.spyOn(global.console, 'warn').mockReturnValue();
+
   it('should throw an error when omitting to pass "propertyNames" to "params"', () => {
     expect(() => iconFormatter(0, 0, 'anything', {} as Column, {}, {} as any))
       .toThrowError('[Slickgrid-Universal] When using `Formatters.icon`, you must provide the "iconCssClass" via the generic "params"');
@@ -22,12 +24,10 @@ describe('the Icon Formatter', () => {
   });
 
   it('should show console warning when using deprecated icon/formatterIcon params', () => {
-    const consoleSpy = jest.spyOn(global.console, 'warn').mockReturnValue();
-
     const input = null;
     const icon = 'fa fa-search';
     const result = iconFormatter(0, 0, input, { field: 'user', params: { icon } } as Column, {}, {} as any);
     expect(result).toBe(`<i class="${icon}" aria-hidden="true"></i>`);
-    expect(consoleSpy).toHaveBeenCalledWith('[Slickgrid-Universal] deprecated params.icon or params.formatterIcon are deprecated when using `Formatters.icon` in favor of params.iconCssClass. (e.g.: `{ formatter: Formatters.icon, params: { iconCssClass: "fa fa-search" }}`');
+    expect(consoleWarnSpy).toHaveBeenCalledWith('[Slickgrid-Universal] deprecated params.icon or params.formatterIcon are deprecated when using `Formatters.icon` in favor of params.iconCssClass. (e.g.: `{ formatter: Formatters.icon, params: { iconCssClass: "fa fa-search" }}`');
   });
 });
