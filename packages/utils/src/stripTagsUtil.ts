8f706fc95f837c6352fb3217952c86b4326f8aaf
ghiscoding <gbeaulac@gmail.com>
2023-12-09 16:47:08 -0500

    fix: `stripTags` shouldn't throw with null/undefined

--- packages/utils/src/stripTagsUtil.ts
+++ packages/utils/src/stripTagsUtil.ts
@@ -39,7 +39,7 @@ export function stripTags(htmlText: string | number | boolean | HTMLElement, all
     if (html instanceof HTMLElement) {
       html = html.innerHTML;
     }
-    if (typeof html !== 'string' && html !== undefined) {
+    if (typeof html !== 'string' && html !== undefined && html !== null) {
       throw new TypeError(`'html' parameter must be a string`);
     }
 
