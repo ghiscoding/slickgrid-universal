af8220881b2791be2cc3f6605eda3955428094c7
Ghislain B <gbeaulac@gmail.com>
2023-12-29 14:59:12 -0500

    feat: make DataView Grouping `compileAccumulatorLoop` CSP safe (#1295)
    * feat: make DataView Grouping `compileAccumulatorLoop` CSP safe

--- packages/common/src/core/__tests__/slickDataView.spec.ts
+++ packages/common/src/core/__tests__/slickDataView.spec.ts
@@ -1,9 +1,11 @@
+import { Aggregators } from '../../aggregators';
+import { Grouping } from '../../interfaces';
 import { SlickDataView } from '../slickDataview';
 import 'flatpickr';
 
 describe('SlickDatView core file', () => {
   let container: HTMLElement;
-  let dataView: SlickDataView;
+  let dv: SlickDataView;
 
   beforeEach(() => {
     container = document.createElement('div');
@@ -13,13 +15,13 @@ describe('SlickDatView core file', () => {
 
   afterEach(() => {
     document.body.textContent = '';
-    dataView.destroy();
+    dv.destroy();
   });
 
   it('should be able to instantiate SlickDataView', () => {
-    dataView = new SlickDataView({});
+    dv = new SlickDataView({});
 
-    expect(dataView.getItems()).toEqual([]);
+    expect(dv.getItems()).toEqual([]);
   });
 
   it('should be able to add items to the DataView', () => {
@@ -27,43 +29,43 @@ describe('SlickDatView core file', () => {
       { id: 1, firstName: 'John', lastName: 'Doe' },
       { id: 2, firstName: 'Jane', lastName: 'Doe' },
     ]
-    dataView = new SlickDataView({});
-    dataView.addItem(mockData[0]);
-    dataView.addItem(mockData[1]);
+    dv = new SlickDataView({});
+    dv.addItem(mockData[0]);
+    dv.addItem(mockData[1]);
 
-    expect(dataView.getLength()).toBe(2);
-    expect(dataView.getItemCount()).toBe(2);
-    expect(dataView.getItems()).toEqual(mockData);
+    expect(dv.getLength()).toBe(2);
+    expect(dv.getItemCount()).toBe(2);
+    expect(dv.getItems()).toEqual(mockData);
   });
 
   describe('batch CRUD methods', () => {
     afterEach(() => {
-      dataView.endUpdate(); // close any batch that weren't closed because of potential error thrown
-      dataView.destroy();
+      dv.endUpdate(); // close any batch that weren't closed because of potential error thrown
+      dv.destroy();
     });
 
     it('should batch items with addItems and begin/end batch update', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
 
-      dataView.beginUpdate(true);
-      dataView.addItems(items);
-      dataView.endUpdate();
+      dv.beginUpdate(true);
+      dv.addItems(items);
+      dv.endUpdate();
 
-      expect(dataView.getIdPropertyName()).toBe('id');
-      expect(dataView.getItems()).toEqual(items);
+      expect(dv.getIdPropertyName()).toBe('id');
+      expect(dv.getItems()).toEqual(items);
     });
 
     it('should batch more items with addItems with begin/end batch update and expect them to be inserted at the end of the dataset', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const newItems = [{ id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 }];
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      dataView.addItems(newItems); // batch extra items
-      dataView.endUpdate();
+      dv.beginUpdate(true);
+      dv.addItems(newItems); // batch extra items
+      dv.endUpdate();
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 },
         { id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 },
       ]);
@@ -73,20 +75,20 @@ describe('SlickDatView core file', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const newItems = [{ id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 }];
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      dataView.insertItems(0, newItems); // batch extra items
-      dataView.endUpdate();
+      dv.beginUpdate(true);
+      dv.insertItems(0, newItems); // batch extra items
+      dv.endUpdate();
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 },
         { id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }
       ]);
 
-      dataView.deleteItem(3);
+      dv.deleteItem(3);
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 4, name: 'Ronald', age: 34 },
         { id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }
       ]);
@@ -95,33 +97,33 @@ describe('SlickDatView core file', () => {
     it('should be able to use different "id" when using setItems()', () => {
       const items = [{ keyId: 0, name: 'John', age: 20 }, { keyId: 1, name: 'Jane', age: 24 }];
 
-      dataView.beginUpdate(true);
-      dataView.setItems(items, 'keyId');
-      dataView.endUpdate();
+      dv.beginUpdate(true);
+      dv.setItems(items, 'keyId');
+      dv.endUpdate();
 
-      expect(dataView.getIdPropertyName()).toBe('keyId');
-      expect(dataView.getItems()).toEqual(items);
+      expect(dv.getIdPropertyName()).toBe('keyId');
+      expect(dv.getItems()).toEqual(items);
     });
 
     it('should batch more items with insertItems with begin/end batch update and expect them to be inserted at a certain index dataset', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const newItems = [{ id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 }];
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      dataView.insertItems(1, newItems); // batch extra items
-      dataView.endUpdate();
+      dv.beginUpdate(true);
+      dv.insertItems(1, newItems); // batch extra items
+      dv.endUpdate();
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'John', age: 20 },
         { id: 3, name: 'Smith', age: 30 }, { id: 4, name: 'Ronald', age: 34 },
         { id: 1, name: 'Jane', age: 24 }
       ]);
 
-      dataView.deleteItems([3, 1]);
+      dv.deleteItems([3, 1]);
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'John', age: 20 },
         { id: 4, name: 'Ronald', age: 34 },
       ]);
@@ -130,37 +132,37 @@ describe('SlickDatView core file', () => {
     it('should throw when trying to delete items with have invalid Ids', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      expect(() => dataView.deleteItems([-1, 1])).toThrow('[SlickGrid DataView] Invalid id');
+      expect(() => dv.deleteItems([-1, 1])).toThrow('[SlickGrid DataView] Invalid id');
     });
 
     it('should throw when trying to delete items with a batch that have invalid Ids', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      expect(() => dataView.deleteItems([-1, 1])).toThrow('[SlickGrid DataView] Invalid id');
+      dv.beginUpdate(true);
+      expect(() => dv.deleteItems([-1, 1])).toThrow('[SlickGrid DataView] Invalid id');
     });
 
     it('should call updateItems, without batch, and expect a refresh to be called', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const updatedItems = [{ id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 }];
-      const refreshSpy = jest.spyOn(dataView, 'refresh');
+      const refreshSpy = jest.spyOn(dv, 'refresh');
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.updateItems(updatedItems.map(item => item.id), updatedItems);
+      dv.updateItems(updatedItems.map(item => item.id), updatedItems);
 
       expect(refreshSpy).toHaveBeenCalled();
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 },
       ]);
 
-      dataView.deleteItem(1);
+      dv.deleteItem(1);
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }
       ]);
     });
@@ -168,22 +170,22 @@ describe('SlickDatView core file', () => {
     it('should batch updateItems and expect a refresh to be called', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const updatedItems = [{ id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 }];
-      const refreshSpy = jest.spyOn(dataView, 'refresh');
+      const refreshSpy = jest.spyOn(dv, 'refresh');
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      dataView.updateItems(updatedItems.map(item => item.id), updatedItems);
+      dv.beginUpdate(true);
+      dv.updateItems(updatedItems.map(item => item.id), updatedItems);
 
       expect(refreshSpy).toHaveBeenCalled();
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 },
       ]);
 
-      dataView.deleteItem(1);
-      dataView.endUpdate();
+      dv.deleteItem(1);
+      dv.endUpdate();
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }
       ]);
     });
@@ -191,22 +193,22 @@ describe('SlickDatView core file', () => {
     it('should batch updateItems and expect a refresh to be called', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const updatedItems = [{ id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 }];
-      const refreshSpy = jest.spyOn(dataView, 'refresh');
+      const refreshSpy = jest.spyOn(dv, 'refresh');
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
-      dataView.updateItems(updatedItems.map(item => item.id), updatedItems);
+      dv.beginUpdate(true);
+      dv.updateItems(updatedItems.map(item => item.id), updatedItems);
 
       expect(refreshSpy).toHaveBeenCalled();
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 },
       ]);
 
-      dataView.deleteItem(1);
-      dataView.endUpdate();
+      dv.deleteItem(1);
+      dv.endUpdate();
 
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'Smith', age: 30 }
       ]);
     });
@@ -214,31 +216,31 @@ describe('SlickDatView core file', () => {
     it('should throw when batching updateItems with some invalid Ids', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const updatedItems = [{ id: 0, name: 'Smith', age: 30 }, { id: 1, name: 'Ronald', age: 34 }];
-      const refreshSpy = jest.spyOn(dataView, 'refresh');
+      const refreshSpy = jest.spyOn(dv, 'refresh');
 
-      dataView.setItems(items); // original items list
+      dv.setItems(items); // original items list
 
-      dataView.beginUpdate(true);
+      dv.beginUpdate(true);
 
-      expect(() => dataView.updateItems([-1, 1], updatedItems)).toThrow('[SlickGrid DataView] Invalid id');
+      expect(() => dv.updateItems([-1, 1], updatedItems)).toThrow('[SlickGrid DataView] Invalid id');
     });
 
     it('should throw when trying to call setItems() with duplicate Ids', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 0, name: 'Jane', age: 24 }];
 
-      expect(() => dataView.setItems(items)).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
+      expect(() => dv.setItems(items)).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
     });
 
     it('should call insertItem() at a defined index location', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const newItem = { id: 2, name: 'Smith', age: 30 };
-      const refreshSpy = jest.spyOn(dataView, 'refresh');
+      const refreshSpy = jest.spyOn(dv, 'refresh');
 
-      dataView.setItems(items);
-      dataView.insertItem(1, newItem);
+      dv.setItems(items);
+      dv.insertItem(1, newItem);
 
       expect(refreshSpy).toHaveBeenCalled();
-      expect(dataView.getItems()).toEqual([
+      expect(dv.getItems()).toEqual([
         { id: 0, name: 'John', age: 20 },
         { id: 2, name: 'Smith', age: 30 },
         { id: 1, name: 'Jane', age: 24 }
@@ -249,8 +251,8 @@ describe('SlickDatView core file', () => {
       const items = [{ id: 0, name: 'John', age: 20 }, { id: 1, name: 'Jane', age: 24 }];
       const newItem = { id: undefined, name: 'Smith', age: 30 };
 
-      dataView.setItems(items);
-      expect(() => dataView.insertItem(1, newItem)).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
+      dv.setItems(items);
+      expect(() => dv.insertItem(1, newItem)).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
     });
 
     it('should throw when trying to call insertItem() with undefined Id', () => {
@@ -259,9 +261,95 @@ describe('SlickDatView core file', () => {
         { id: 1, name: 'Jane', age: 24 },
         { id: undefined, name: 'Smith', age: 30 }];
 
-      dataView.beginUpdate(true);
-      dataView.setItems(items);
-      expect(() => dataView.endUpdate()).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
+      dv.beginUpdate(true);
+      dv.setItems(items);
+      expect(() => dv.endUpdate()).toThrow(`[SlickGrid DataView] Each data element must implement a unique 'id' property`);
+    });
+  });
+
+  describe('Grouping', () => {
+    it('should call setGrouping() and expect grouping to be defined without any accumulator neither totals when Aggregators are omitted', () => {
+      const mockData = [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Jane', lastName: 'Doe' },
+      ]
+      dv = new SlickDataView({});
+      const refreshSpy = jest.spyOn(dv, 'refresh');
+      dv.setItems(mockData);
+
+      const agg = new Aggregators.Sum('lastName');
+      dv.setGrouping({
+        getter: 'lastName',
+        formatter: (g) => `Family: ${g.value} <span class="text-green">(${g.count} items)</span>`,
+      } as Grouping);
+
+      expect(refreshSpy).toHaveBeenCalled();
+      expect(dv.getGrouping().length).toBe(1);
+      expect(dv.getGrouping()[0]).toMatchObject({ aggregators: [], getter: 'lastName' });
+
+      expect(dv.getItem(0)).toEqual({
+        __group: true,
+        __nonDataRow: true,
+        collapsed: 0,
+        count: 2,
+        groupingKey: 'Doe',
+        groups: null,
+        level: 0,
+        rows: mockData,
+        selectChecked: false,
+        title: 'Family: Doe <span class="text-green">(2 items)</span>',
+        totals: null,
+        value: 'Doe'
+      });
+      expect(dv.getItem(1)).toEqual(mockData[0]);
+      expect(dv.getItem(2)).toEqual(mockData[1]);
+      expect(dv.getItem(3)).toBeUndefined(); // without Totals
+    });
+
+    it('should call setGrouping() and expect grouping to be defined with compiled accumulator and totals when providing Aggregators', () => {
+      const mockData = [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Jane', lastName: 'Doe' },
+      ]
+      dv = new SlickDataView({});
+      const refreshSpy = jest.spyOn(dv, 'refresh');
+      dv.setItems(mockData);
+
+      const agg = new Aggregators.Sum('lastName');
+      dv.setGrouping({
+        getter: 'lastName',
+        formatter: (g) => `Family: ${g.value} <span class="text-green">(${g.count} items)</span>`,
+        aggregators: [agg],
+        aggregateCollapsed: false,
+      } as Grouping);
+
+      expect(refreshSpy).toHaveBeenCalled();
+      expect(dv.getGrouping().length).toBe(1);
+      expect(dv.getGrouping()[0]).toMatchObject({ aggregators: [agg], getter: 'lastName' });
+
+      expect(dv.getItem(0)).toEqual({
+        __group: true,
+        __nonDataRow: true,
+        collapsed: 0,
+        count: 2,
+        groupingKey: 'Doe',
+        groups: null,
+        level: 0,
+        rows: mockData,
+        selectChecked: false,
+        title: 'Family: Doe <span class="text-green">(2 items)</span>',
+        totals: expect.anything(),
+        value: 'Doe'
+      });
+      expect(dv.getItem(1)).toEqual(mockData[0]);
+      expect(dv.getItem(2)).toEqual(mockData[1]);
+      expect(dv.getItem(3)).toEqual({
+        __groupTotals: true,
+        __nonDataRow: true,
+        group: expect.anything(),
+        initialized: true,
+        sum: { lastName: 0 }
+      });
     });
   });
 });
