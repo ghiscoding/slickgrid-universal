6a0a9ec016959501a5fa9665a2fdbbe538ad8e68
Vildan Softic <zewa666@gmail.com>
2024-02-07 20:40:31 +0100

    chore: one more linter fix

--- packages/common/src/core/slickCore.ts
+++ packages/common/src/core/slickCore.ts
@@ -447,7 +447,7 @@ export class SlickGroup extends SlickNonDataItem {
    * @property groupingKey
    * @type {Object}
    */
-  groupingKey: any = null;
+  groupingKey = null;
 
   constructor() {
     super();
