4ed241b7f5c00791e85986bfcef238a507f8c280
Ghislain B <gbeaulac@gmail.com>
2024-02-12 17:05:15 -0500

    chore: migrate from node-archiver to fflate to zip standalone SF bundle (#1387)
    * chore: replace bundle zip creation from `node-archiver` to `fflate`

--- packages/vanilla-force-bundle/compress.mjs
+++ packages/vanilla-force-bundle/compress.mjs
@@ -1,8 +1,10 @@
 
-import fs from 'fs';
-import archiver from 'archiver';
-import yargs from 'yargs';
+import { strToU8, zip, gzip, zlib, zlibSync } from 'fflate';
+import { globSync } from 'glob';
+import { existsSync, mkdirSync, readFileSync, statSync, writeFileSync} from 'node:fs';
+import normalizePath from 'normalize-path';
 import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
 
 const inputFolder1 = './dist/bundle';
 const inputFolder2 = '../common/dist/styles';
@@ -10,25 +12,54 @@ const argv = yargs(hideBin(process.argv)).argv;
 const outputFilename = argv.outputFilename || 'bundle';
 const outputFolder = argv.outputFolder || './dist/';
 
-const ext = 'zip';
-const archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
-});
-
-if (!fs.existsSync(outputFolder)) {
-  fs.mkdirSync(outputFolder);
+if (!existsSync(outputFolder)) {
+  mkdirSync(outputFolder);
 }
 
-const outputPathFilename = `${outputFolder}${outputFilename}.${ext}`;
+// get all files from `dist/bundle`
+const bundleFiles = globSync('./dist/bundle/**/*.*');
+const files = [
+  { name: 'slickgrid-vanilla-bundle.js', path: bundleFiles[0] },
+  { name: 'package.json', path: './package.json' },
+];
 
-archive.pipe(fs.createWriteStream(outputPathFilename));
-archive.directory(inputFolder1, false);
-archive.directory(inputFolder2, 'styles');
-archive.file('package.json', { name: 'package.json' });
-archive.finalize().then(() => {
-  console.log(`Compressed input folders "${inputFolder1}" and "${inputFolder2}" to single output file "${outputPathFilename}"`);
-  console.log(`File Location:: "${import.meta.url.replace(/\\/gi, '/')}/${outputPathFilename}"`);
-  console.log(`File Size:: ${(fs.statSync(outputPathFilename).size / 1024).toFixed(2)}Kb`);
-  console.log(`Processed Timestamp`, new Date().toLocaleString('en-CA'));
-  console.log(`ALL DONE!!!`);
+// get all files from `common/dist/styles`
+const styleFiles = globSync('../common/dist/styles/**/*.*');
+styleFiles.forEach(file => {
+  const [styleName] = file.match(/(styles.*)/gi) || [];
+  files.push({ name: normalizePath(styleName), path: normalizePath(file) });
 });
+
+let zipObj = {}; // create an object tree of the zip folders/files structure
+let left = files.length;
+const fileToU8 = (file, cb) => cb(strToU8(readFileSync(file.path)));
+
+// Yet again, this is necessary for parallelization.
+let processFile = (file) => {
+  fileToU8(file, (buffer) => {
+    zipObj[file.name] = buffer;
+
+    if (!--left) {
+      // use in fflate zip (sync mode) to take full advantage of Web Workers
+      // compress to level 9 (highest)
+      zip(zipObj, { level: 9 }, (err, out) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const outputPathFilename = `${outputFolder}${outputFilename}.zip`;
+          writeFileSync(outputPathFilename, out);
+
+          console.log(`Compressed input folders "${inputFolder1}" and "${inputFolder2}" to single output file "${outputPathFilename}"`);
+          console.log(`File Location:: "${import.meta.url.replace(/\\/gi, '/')}/${outputPathFilename}"`);
+          console.log(`File Size:: ${(statSync(outputPathFilename).size / 1024).toFixed(2)}Kb`);
+          console.log(`Processed Timestamp`, new Date().toLocaleString('en-CA'));
+          console.log(`ALL DONE!!!`);
+        }
+      });
+    }
+  });
+};
+
+for (let file of files) {
+  processFile(file);
+}
