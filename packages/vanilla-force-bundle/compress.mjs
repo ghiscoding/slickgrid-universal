511a36d0ed803cb017a419a967fbe4a747d78151
ghiscoding <Ghislain.Beaulac@se.com>
2024-02-12 16:50:22 -0500

    chore: migrate from `node-archiver` to `fflate` - remove old node-archiver implementation - NOTE: the new zip file is compatible with the previous implementation BUT it is a bit larger when using `fflate` (about 20Kb larger) - the larger file was explained in `fflate` GitHub Discussion: https://github.com/101arrowz/fflate/discussions/195 - the main reason to use `fflate` instead of `node-archiver` is because our new `Excel-Builder-Vanilla` repo uses `fflate` and also because `fflate` is ESM and `node-archiver` is not and wasn't updated in a while, so even if the zip is slightly larger switching to fflate is still an improvement and doesn't really matter for Salesforce - new bundled zip file was also tested in Salesforce

--- packages/vanilla-force-bundle/compress.mjs
+++ packages/vanilla-force-bundle/compress.mjs
@@ -1,11 +1,10 @@
 
-import fs from 'fs';
-import archiver from 'archiver';
 import { strToU8, zip, gzip, zlib, zlibSync } from 'fflate';
 import { globSync } from 'glob';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { existsSync, mkdirSync, readFileSync, statSync, writeFileSync} from 'node:fs';
 import normalizePath from 'normalize-path';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
 
 const inputFolder1 = './dist/bundle';
 const inputFolder2 = '../common/dist/styles';
@@ -13,46 +12,27 @@ const argv = yargs(hideBin(process.argv)).argv;
 const outputFilename = argv.outputFilename || 'bundle';
 const outputFolder = argv.outputFolder || './dist/';
 
-const ext = 'zip';
-const archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
-});
-
-if (!fs.existsSync(outputFolder)) {
-  fs.mkdirSync(outputFolder);
+if (!existsSync(outputFolder)) {
+  mkdirSync(outputFolder);
 }
 
-const outputPathFilename = `${outputFolder}${outputFilename}.${ext}`;
-
-archive.pipe(fs.createWriteStream(outputPathFilename));
-archive.directory(inputFolder1, false);
-archive.directory(inputFolder2, 'styles');
-archive.file('package.json', { name: 'package.json' });
-archive.finalize().then(() => {
-  console.log(`Compressed input folders "${inputFolder1}" and "${inputFolder2}" to single output file "${outputPathFilename}"`);
-  console.log(`File Location:: "${import.meta.url.replace(/\\/gi, '/')}/${outputPathFilename}"`);
-  console.log(`File Size:: ${(fs.statSync(outputPathFilename).size / 1024).toFixed(2)}Kb`);
-  console.log(`Processed Timestamp`, new Date().toLocaleString('en-CA'));
-  console.log(`ALL DONE!!!`);
-});
-
-// All files from `dist/bundle`
+// get all files from `dist/bundle`
 const bundleFiles = globSync('./dist/bundle/**/*.*');
 const files = [
   { name: 'slickgrid-vanilla-bundle.js', path: bundleFiles[0] },
   { name: 'package.json', path: './package.json' },
 ];
 
-// All files from `common/dist/styles`
+// get all files from `common/dist/styles`
 const styleFiles = globSync('../common/dist/styles/**/*.*');
 styleFiles.forEach(file => {
   const [styleName] = file.match(/(styles.*)/gi) || [];
   files.push({ name: normalizePath(styleName), path: normalizePath(file) });
 });
 
-let left = files.length;
 let zipObj = {}; // create an object tree of the zip folders/files structure
-const fileToU8 = (file, cb) => cb(strToU8(fs.readFileSync(file.path)));
+let left = files.length;
+const fileToU8 = (file, cb) => cb(strToU8(readFileSync(file.path)));
 
 // Yet again, this is necessary for parallelization.
 let processFile = (file) => {
@@ -60,16 +40,20 @@ let processFile = (file) => {
     zipObj[file.name] = buffer;
 
     if (!--left) {
-      // use in synchronouse mode to perform execution in multiple thread
-      zip(zipObj, {
-        mem: 0,
-        level: 9 // compression level
-      }, function (err, out) {
+      // use in fflate zip (sync mode) to take full advantage of Web Workers
+      // compress to level 9 (highest)
+      zip(zipObj, { level: 9 }, (err, out) => {
         if (err) {
-          console.log(err);
+          console.error(err);
         } else {
-          console.log('Length ', out.length);
-          fs.writeFileSync(`${outputFolder}${outputFilename}2.zip`, out);
+          const outputPathFilename = `${outputFolder}${outputFilename}.zip`;
+          writeFileSync(outputPathFilename, out);
+
+          console.log(`Compressed input folders "${inputFolder1}" and "${inputFolder2}" to single output file "${outputPathFilename}"`);
+          console.log(`File Location:: "${import.meta.url.replace(/\\/gi, '/')}/${outputPathFilename}"`);
+          console.log(`File Size:: ${(statSync(outputPathFilename).size / 1024).toFixed(2)}Kb`);
+          console.log(`Processed Timestamp`, new Date().toLocaleString('en-CA'));
+          console.log(`ALL DONE!!!`);
         }
       });
     }
