28016489ae6598e439abfc3617db81ed0895eaf9
ghiscoding <Ghislain.Beaulac@se.com>
2024-02-14 16:15:31 -0500

    chore: change Husky commitlint body size to unlimited - I don't care how long the body is but I do care about how long the PR title is (max 72)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [1, 'always', 72],
+    'footer-max-line-length': [0, 'always'] // Make sure there is never a max-line-length by disabling the rule
+  },
+};
