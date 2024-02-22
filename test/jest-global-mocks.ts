61e99a4fb5aa41c86462fac4729aabb6245ae037
ghiscoding <gbeaulac@gmail.com>
2024-02-21 21:07:39 -0500

    test: add missing JSDOM scrollIntoView prototype function

--- test/jest-global-mocks.ts
+++ test/jest-global-mocks.ts
@@ -8,6 +8,8 @@ const mock = () => {
   };
 };
 
+window.HTMLElement.prototype.scrollIntoView = () => { };
+
 Object.defineProperty(window, 'localStorage', { value: mock() });
 Object.defineProperty(window, 'sessionStorage', { value: mock() });
 Object.defineProperty(window, 'getComputedStyle', {
