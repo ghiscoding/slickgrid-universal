aa4c6d0ba1a09bbe2aea6ebfeb8f77ab7a0908c1
Vildan Softic <zewa666@gmail.com>
2024-04-09 23:31:31 +0200

    feat: notify onValidationError on paste if validation failed

--- packages/common/src/extensions/slickCellExternalCopyManager.ts
+++ packages/common/src/extensions/slickCellExternalCopyManager.ts
@@ -1,6 +1,6 @@
 import { createDomElement, getHtmlStringOutput, stripTags } from '@slickgrid-universal/utils';
 
-import type { Column, ExcelCopyBufferOption, ExternalCopyClipCommand, OnEventArgs } from '../interfaces/index';
+import type { Column, Editor, ExcelCopyBufferOption, ExternalCopyClipCommand, OnEventArgs } from '../interfaces/index';
 import { SlickEvent, SlickEventData, SlickEventHandler, type SlickGrid, SlickRange, type SlickDataView, Utils as SlickUtils } from '../core/index';
 
 // using external SlickGrid JS libraries
@@ -127,15 +127,15 @@ export class SlickCellExternalCopyManager {
 
     // if a custom getter is not defined, we call serializeValue of the editor to serialize
     if (columnDef) {
-      if (columnDef.editor) {
+      if (columnDef.editor?.model) {
         const tmpP = document.createElement('p');
-        const editor = new (columnDef as any).editor({
+        const editor = new (columnDef as any).editor.model({
           container: tmpP,  // a dummy container
           column: columnDef,
           event,
           position: { top: 0, left: 0 },  // a dummy position required by some editors
           grid: this._grid,
-        });
+        }) as Editor;
         editor.loadValue(item);
         retVal = editor.serializeValue();
         editor.destroy();
@@ -155,15 +155,29 @@ export class SlickCellExternalCopyManager {
       }
 
       // if a custom setter is not defined, we call applyValue of the editor to unserialize
-      if (columnDef.editor) {
+      if (columnDef.editor?.model) {
         const tmpDiv = document.createElement('div');
-        const editor = new (columnDef as any).editor({
+        const editor = new (columnDef as any).editor.model({
           container: tmpDiv, // a dummy container
           column: columnDef,
           position: { top: 0, left: 0 },  // a dummy position required by some editors
           grid: this._grid
-        });
+        }) as Editor;
         editor.loadValue(item);
+        const validationResults = editor.validate(undefined, value);
+        if (!validationResults.valid) {
+          const activeCell = this._grid.getActiveCell()!;
+          this._grid.onValidationError.notify({
+            editor,
+            cellNode: this._grid.getActiveCellNode()!,
+            validationResults,
+            row: activeCell?.row,
+            cell: activeCell?.cell,
+            column: columnDef,
+            grid: this._grid,
+          });
+        }
+
         editor.applyValue(item, value);
         editor.destroy();
         tmpDiv.remove();
