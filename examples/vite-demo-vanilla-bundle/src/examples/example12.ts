f108b7d4b2eb5a65b6dbb04147c9c8b0ea0b8200
ghiscoding <gbeaulac@gmail.com>
2024-04-28 02:03:01 -0400

    chore(styling): add CSS variable for .slick-cell optional flex

--- examples/vite-demo-vanilla-bundle/src/examples/example12.ts
+++ examples/vite-demo-vanilla-bundle/src/examples/example12.ts
@@ -365,7 +365,8 @@ export default class Example12 {
       {
         id: 'action', name: 'Action', field: 'action', width: 70, minWidth: 70, maxWidth: 70,
         excludeFromExport: true,
-        formatter: () => `<div class="button-style margin-auto action-btn"><span class="mdi mdi-dots-vertical mdi-22px text-color-primary"></span></div>`,
+        cssClass: 'justify-center flex',
+        formatter: () => `<div class="button-style action-btn"><span class="mdi mdi-dots-vertical mdi-22px text-color-primary"></span></div>`,
         cellMenu: {
           hideCloseButton: false,
           commandTitle: 'Commands',
