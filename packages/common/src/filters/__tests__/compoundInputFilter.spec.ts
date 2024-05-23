50fac0b6d4acbfed4b03db295c168b15373e77e9
ghiscoding <Ghislain.Beaulac@se.com>
2024-05-23 10:42:34 -0400

    chore: add more unit tests to cover changes

--- packages/common/src/filters/__tests__/compoundInputFilter.spec.ts
+++ packages/common/src/filters/__tests__/compoundInputFilter.spec.ts
@@ -413,6 +413,35 @@ describe('CompoundInputFilter', () => {
     expect(removeExtraSpaces(filterOperatorElm[0][5].textContent!)).toBe('*z Ends With');
   });
 
+  it('should have custom compound operator list including alternate texts and show up in the operator select dropdown options list', () => {
+    mockColumn.outputType = null as any;
+    filterArguments.searchTerms = ['xyz'];
+    mockColumn.filter!.compoundOperatorList = [
+      { operator: '', desc: '' },
+      { operator: '=', desc: 'Equal to' },
+      { operator: '<', desc: 'Less than' },
+      { operator: '>', desc: 'Greater than' },
+      { operator: 'Custom', desc: 'SQL LIKE' },
+    ];
+    jest.spyOn(gridStub, 'getOptions').mockReturnValue({
+      ...gridOptionMock, compoundOperatorAltTexts: {
+        text: {
+          '=': { operatorAlt: 'eq', descAlt: 'alternate numeric equal description' },
+          'Custom': { operatorAlt: '%', descAlt: 'alternate SQL LIKE' }
+        }
+      }
+    });
+
+    filter.init(filterArguments);
+    const filterOperatorElm = divContainer.querySelectorAll<HTMLSelectElement>('.search-filter.filter-duration select');
+
+    expect(filterOperatorElm[0][0].title).toBe('');
+    expect(removeExtraSpaces(filterOperatorElm[0][1].textContent!)).toBe('eq alternate numeric equal description');
+    expect(removeExtraSpaces(filterOperatorElm[0][2].textContent!)).toBe('< Less than');
+    expect(removeExtraSpaces(filterOperatorElm[0][3].textContent!)).toBe('> Greater than');
+    expect(removeExtraSpaces(filterOperatorElm[0][4].textContent!)).toBe('% alternate SQL LIKE');
+  });
+
   describe('with French I18N translations', () => {
     beforeEach(() => {
       gridOptionMock.enableTranslate = true;
