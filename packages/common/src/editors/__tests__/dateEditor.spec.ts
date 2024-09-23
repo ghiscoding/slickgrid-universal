83188534676a403f3edd548ff2f5dec2b0344c73
Vildan Softic <zewa666@gmail.com>
2024-09-23 20:49:13 +0200

    fix: allowInput instead of allowEdit
    https://github.com/ghiscoding/slickgrid-universal/issues/1684

--- packages/common/src/editors/__tests__/dateEditor.spec.ts
+++ packages/common/src/editors/__tests__/dateEditor.spec.ts
@@ -147,15 +147,15 @@ describe('DateEditor', () => {
       const event = new KeyboardEvent('keydown', { key: 'Enter' });
       editor.editorDomElement.dispatchEvent(event);
 
-      expect(editor.columnEditor.editorOptions?.allowEdit).toBeFalsy();
+      expect(editor.columnEditor.editorOptions?.allowInput).toBeFalsy();
       expect(editor.isValueTouched()).toBeFalsy();
     });
 
-    it('should stop propagation on allowEdit when hitting left or right arrow keys', () => {
+    it('should stop propagation on allowInput when hitting left or right arrow and home and end keys', () => {
       editor = new DateEditor({ ...editorArguments,
         column: { ...editorArguments.column,
           editor: { ...editorArguments.column.editor,
-            editorOptions: { ...editorArguments.column?.editor?.editorOptions, allowEdit: true }
+            editorOptions: { ...editorArguments.column?.editor?.editorOptions, allowInput: true }
            }}});
 
       let event = new KeyboardEvent('keydown', { key: 'ArrowLeft' });
@@ -300,7 +300,7 @@ describe('DateEditor', () => {
 
         editor = new DateEditor({...editorArguments,
           column: { ...mockColumn, editor: { ...editorArguments.column.editor, alwaysSaveOnEnterKey: true,
-            editorOptions: { ...editorArguments.column.editor?.editorOptions, allowEdit: true}
+            editorOptions: { ...editorArguments.column.editor?.editorOptions, allowInput: true}
            } }
         });
         const event = new KeyboardEvent('keydown', { key: 'Enter' });
