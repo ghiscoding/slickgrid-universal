9774f7b64b7e7437e7580b08663df94530c1a4f1
ghiscoding <gbeaulac@gmail.com>
2024-10-08 23:48:02 -0400

    chore: use `node:` import namespace & replace glob by tinyglobby

--- packages/vanilla-force-bundle/compress.mjs
+++ packages/vanilla-force-bundle/compress.mjs
@@ -1,8 +1,8 @@
 
 import { strToU8, zip } from 'fflate';
-import { globSync } from 'glob';
 import { existsSync, mkdirSync, readFileSync, statSync, writeFileSync } from 'node:fs';
 import normalizePath from 'normalize-path';
+import { globSync } from 'tinyglobby';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs';
 
