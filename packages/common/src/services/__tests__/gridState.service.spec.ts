f5cc5870798bea23f69e75479a77575647837754
ghiscoding <gbeaulac@gmail.com>
2024-11-08 23:39:22 -0500

    fix: hiding column(s) returned incorrect Grid State changes data

--- packages/common/src/services/__tests__/gridState.service.spec.ts
+++ packages/common/src/services/__tests__/gridState.service.spec.ts
@@ -34,7 +34,13 @@ vi.useFakeTimers();
 const fnCallbacks = {};
 const mockPubSub = {
   publish: vi.fn(),
-  subscribe: (eventName, fn) => fnCallbacks[eventName as string] = fn,
+  subscribe: (eventName, fn) => {
+    if (Array.isArray(eventName)) {
+      eventName.forEach(ev => fnCallbacks[ev as string] = fn);
+    } else {
+      fnCallbacks[eventName as string] = fn;
+    }
+  },
   unsubscribe: vi.fn(),
   unsubscribeAll: vi.fn(),
 } as BasePubSubService;
@@ -944,6 +950,22 @@ describe('GridStateService', () => {
       const getAssocCurColSpy = vi.spyOn(service, 'getAssociatedCurrentColumns').mockReturnValue(currentColumnsMock1);
 
       fnCallbacks['onHeaderMenuHideColumns'](columnsMock1);
+
+      expect(getCurGridStateSpy).toHaveBeenCalled();
+      expect(getAssocCurColSpy).toHaveBeenCalled();
+      expect(pubSubSpy).toHaveBeenCalledWith(`onGridStateChanged`, stateChangeMock);
+    });
+
+    it('should trigger a "onGridStateChanged" event when "onHideColumns" is triggered', () => {
+      const columnsMock1 = [{ id: 'field1', field: 'field1', width: 100, cssClass: 'red' }] as Column[];
+      const currentColumnsMock1 = [{ columnId: 'field1', cssClass: 'red', headerCssClass: '', width: 100 }] as CurrentColumn[];
+      const gridStateMock = { columns: currentColumnsMock1, filters: [], sorters: [] } as GridState;
+      const stateChangeMock = { change: { newValues: currentColumnsMock1, type: GridStateType.columns }, gridState: gridStateMock } as GridStateChange;
+      const pubSubSpy = vi.spyOn(mockPubSub, 'publish');
+      const getCurGridStateSpy = vi.spyOn(service, 'getCurrentGridState').mockReturnValue(gridStateMock);
+      const getAssocCurColSpy = vi.spyOn(service, 'getAssociatedCurrentColumns').mockReturnValue(currentColumnsMock1);
+
+      fnCallbacks['onHideColumns'](columnsMock1);
 
       expect(getCurGridStateSpy).toHaveBeenCalled();
       expect(getAssocCurColSpy).toHaveBeenCalled();
