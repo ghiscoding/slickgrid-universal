2ce89f5a7494d25ba29da12e6371f46d264456b9
Ghislain B. <gbeaulac@gmail.com>
2024-11-27 09:52:01 -0500

    chore: remove unnecessary subscribe arguments

--- packages/common/src/extensions/slickCellExternalCopyManager.ts
+++ packages/common/src/extensions/slickCellExternalCopyManager.ts
@@ -81,7 +81,7 @@ export class SlickCellExternalCopyManager {
 
     // we give focus on the grid when a selection is done on it (unless it's an editor, if so the editor should have already set focus to the grid prior to editing a cell).
     // without this, if the user selects a range of cell without giving focus on a particular cell, the grid doesn't get the focus and key stroke handles (ctrl+c) don't work
-    this._eventHandler.subscribe(cellSelectionModel.onSelectedRangesChanged, (_e, ranges) => {
+    this._eventHandler.subscribe(cellSelectionModel.onSelectedRangesChanged, () => {
       if (!this._grid.getEditorLock().isActive() && !document.activeElement?.classList.contains('slick-filter')) {
         this._grid.focus();
       }
