5a135b8fddb202f70135b8b8b468384457431321
ghiscoding <gbeaulac@gmail.com>
2025-02-02 00:16:43 -0500

    chore: fix failing Cypress tests

--- demos/vanilla/src/examples/example25.ts
+++ demos/vanilla/src/examples/example25.ts
@@ -202,9 +202,8 @@ export default class Example25 {
       },
     ];
 
-    const today = new Date();
     const presetLowestDay = format(addDay(new Date(), -2), 'YYYY-MM-DD');
-    const presetHighestDay = format(addDay(new Date(), today.getDate() < 14 ? 28 : 25), 'YYYY-MM-DD');
+    const presetHighestDay = format(addDay(new Date(), 25), 'YYYY-MM-DD');
 
     this.gridOptions = {
       autoResize: {
