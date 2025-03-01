4f89958681376be6288f596fb6cf6f448ca0be48
ghiscoding <gbeaulac@gmail.com>
2025-03-01 14:35:27 -0500

    fix: Header Menu should reposition correct even with row detail grids

--- packages/common/src/extensions/slickHeaderMenu.ts
+++ packages/common/src/extensions/slickHeaderMenu.ts
@@ -145,8 +145,9 @@ export class SlickHeaderMenu extends MenuBaseClass<HeaderMenu> {
     const isSubMenu = menuElm.classList.contains('slick-submenu');
     const parentElm = isSubMenu ? ((e.target as HTMLElement).closest('.slick-menu-item') as HTMLDivElement) : (buttonElm as HTMLElement);
 
-    const containerElm: HTMLElement =
-      this.sharedService.gridContainerElement.querySelector('.slickgrid-container') ?? this.sharedService.gridContainerElement;
+    const containerElm: HTMLElement = this.sharedService.gridContainerElement.classList.contains('slickgrid-container')
+      ? this.sharedService.gridContainerElement
+      : (this.sharedService.gridContainerElement.querySelector('.slickgrid-container') ?? this.sharedService.gridContainerElement);
     const relativePos = getOffsetRelativeToParent(containerElm, buttonElm);
     const gridPos = this.grid.getGridPosition();
     const menuWidth = menuElm.offsetWidth;
