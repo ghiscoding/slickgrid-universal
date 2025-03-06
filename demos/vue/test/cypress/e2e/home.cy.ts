22b6bbc4900b6e3b44a283ec343548cd5fda179a
ghiscoding <gbeaulac@gmail.com>
2025-03-06 00:51:13 -0500

    chore: update home page & tests

--- demos/vue/test/cypress/e2e/home.cy.ts
+++ demos/vue/test/cypress/e2e/home.cy.ts
@@ -3,9 +3,5 @@ describe('Home Page', () => {
     cy.visit(`${Cypress.config('baseUrl')}/home`);
 
     cy.get('h2').should('contain', 'Slickgrid-Vue');
-
-    cy.get('h4').contains('Quick intro');
-
-    cy.get('h4').contains('Documentation');
   });
 });
