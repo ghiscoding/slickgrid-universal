54a5a33ee1e1f013679b6570247f5159aa5504d3
ghiscoding <gbeaulac@gmail.com>
2025-05-01 18:16:28 -0400

    chore: fix missing types for SortableJS ESM build

--- packages/common/typings/typings.d.ts
+++ packages/common/typings/typings.d.ts
@@ -6,3 +6,8 @@ interface NodeModule {
 interface window {
   Slicker: any;
 }
+
+declare module 'sortablejs/modular/sortable.core.esm.js' {
+  import Sortable from 'sortablejs';
+  export default Sortable;
+}
