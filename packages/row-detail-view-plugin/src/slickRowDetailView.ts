150757d1bd07ef4843a7803af16015ddb061c9cb
Carlos Del Rosario <carlosdelros@gmail.com>
2025-06-25 21:51:06 -0400

    fix: make sure Row Detail item exists before trying to access it (#2026)
    * fix: make sure Row Detail item exists before trying to access it
    
    This Pull Request adds a check to ensure item exists before trying to access a property in it.

--- packages/row-detail-view-plugin/src/slickRowDetailView.ts
+++ packages/row-detail-view-plugin/src/slickRowDetailView.ts
@@ -784,22 +784,25 @@ export class SlickRowDetailView implements ExternalResource, UniversalRowDetailV
 
   protected handleRemoveRow(rowIndex: number): void {
     const item = this.dataView.getItemByIdx(rowIndex);
-    const rowId = item[this.dataViewIdProperty];
 
-    if (this._expandedRowIds.has(rowId)) {
-      this.onBeforeRowOutOfViewportRange.notify(
-        {
-          grid: this._grid,
-          item,
-          rowId,
-          rowIndex,
-          expandedRows: Array.from(this._expandedRowIds).map((id) => this.dataView.getItemById(id)),
-          rowIdsOutOfViewport: Array.from(this.syncOutOfViewportArray(rowId, true)),
-        },
-        null,
-        this
-      );
-      this._disposedRows.add(rowIndex);
+    if (item) {
+      const rowId = item[this.dataViewIdProperty];
+
+      if (this._expandedRowIds.has(rowId)) {
+        this.onBeforeRowOutOfViewportRange.notify(
+          {
+            grid: this._grid,
+            item,
+            rowId,
+            rowIndex,
+            expandedRows: Array.from(this._expandedRowIds).map((id) => this.dataView.getItemById(id)),
+            rowIdsOutOfViewport: Array.from(this.syncOutOfViewportArray(rowId, true)),
+          },
+          null,
+          this
+        );
+        this._disposedRows.add(rowIndex);
+      }
     }
   }
 
