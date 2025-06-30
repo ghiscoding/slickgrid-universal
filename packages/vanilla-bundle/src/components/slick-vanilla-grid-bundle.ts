d471593235518f5dd26c5f239e3c2cf58d1c2398
ghiscoding <gbeaulac@gmail.com>
2025-06-29 23:56:00 -0400

    chore: fix failing unit tests

--- packages/vanilla-bundle/src/components/slick-vanilla-grid-bundle.ts
+++ packages/vanilla-bundle/src/components/slick-vanilla-grid-bundle.ts
@@ -345,9 +345,6 @@ export class SlickVanillaGridBundle<TData = any> {
     this._gridOptions = this.mergeGridOptions(options || {});
     const isDeepCopyDataOnPageLoadEnabled = !!this._gridOptions?.enableDeepCopyDatasetOnPageLoad;
 
-    this.sharedService = services?.sharedService ?? new SharedService();
-    this.sharedService.gridOptions = this._gridOptions;
-
     // add dark mode CSS class when enabled
     if (this._gridOptions.darkMode) {
       this.setDarkMode(true);
@@ -365,6 +362,7 @@ export class SlickVanillaGridBundle<TData = any> {
     const slickgridConfig = new SlickgridConfig();
     this.backendUtilityService = services?.backendUtilityService ?? new BackendUtilityService();
     this.gridEventService = services?.gridEventService ?? new GridEventService();
+    this.sharedService = services?.sharedService ?? new SharedService();
     this.collectionService = services?.collectionService ?? new CollectionService(this.translaterService);
     this.extensionUtility =
       services?.extensionUtility ?? new ExtensionUtility(this.sharedService, this.backendUtilityService, this.translaterService);
