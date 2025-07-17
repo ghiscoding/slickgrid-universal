325ffe9af9bc5351d52e8f58b0a73bcc88675a71
ghiscoding <gbeaulac@gmail.com>
2025-07-17 19:02:40 -0400

    chore: remove added grid options to fix E2E tests

--- demos/vanilla/src/examples/example07.ts
+++ demos/vanilla/src/examples/example07.ts
@@ -423,10 +423,6 @@ export default class Example07 {
           dataContextIds: [2, 3, 6, 7], // (recommended) select by your data object IDs
         },
       },
-      headerMenu: {
-        hideFreezeColumnsCommand: false,
-        subItemChevronClass: 'mdi mdi-chevron-down mdi-rotate-270',
-      },
     };
   }
 
