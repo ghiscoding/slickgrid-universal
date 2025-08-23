acc7bd8548136dbe6515b35c50b7b1cfa63523b9
ghiscoding <gbeaulac@gmail.com>
2025-08-23 13:21:16 -0400

    chore: tweak Cypress test to avoid flakiness

--- test/cypress/e2e/example33.cy.ts
+++ test/cypress/e2e/example33.cy.ts
@@ -43,7 +43,10 @@ describe('Example 33 - Column & Row Span', { retries: 0 }, () => {
       expect(parseInt(`${$el.outerHeight()}`, 10)).to.eq(GRID_ROW_HEIGHT * 3)
     );
 
-    cy.get('.slick-header-columns').children('.slick-header-column:nth(0)').contains('Title').drag('.slick-header-column:nth(1)');
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth-of-type(1)')
+      .contains('Title')
+      .drag('.slick-header-column:nth-of-type(2)');
     cy.get('.slick-header-column:nth(0)').contains('Revenue Growth');
     cy.get('.slick-header-column:nth(1)').contains('Title');
 
@@ -68,7 +71,10 @@ describe('Example 33 - Column & Row Span', { retries: 0 }, () => {
     cy.get(`[style*="top: ${GRID_ROW_HEIGHT * 0}px;"] > .slick-cell.l1.r1.rowspan`).should(($el) =>
       expect(parseInt(`${$el.outerHeight()}`, 10)).to.eq(GRID_ROW_HEIGHT * 3)
     );
-    cy.get('.slick-header-columns').children('.slick-header-column:nth(0)').contains('Revenue Growth').drag('.slick-header-column:nth(1)');
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth-of-type(1)')
+      .contains('Revenue Growth')
+      .drag('.slick-header-column:nth-of-type(2)');
     cy.get('.slick-header-column:nth(0)').contains('Title');
     cy.get('.slick-header-column:nth(1)').contains('Revenue Growth');
 
