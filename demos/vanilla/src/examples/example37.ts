1fd860e00904abb7f5bb9ed766772e537aa63c5e
ghiscoding <gbeaulac@gmail.com>
2025-09-24 18:37:00 -0400

    chore: rename and shift Example names to avoid conflicts

--- demos/vanilla/src/examples/example37.ts
+++ demos/vanilla/src/examples/example37.ts
@@ -1,129 +1,190 @@
 import {
   type Column,
   Editors,
+  Formatters,
   type GridOption,
-  // Handler,
-  // OnDragReplaceCellsEventArgs,
   SlickEventHandler,
   SlickHybridSelectionModel,
-  SlickSelectionUtils,
 } from '@slickgrid-universal/common';
+import { ExcelExportService } from '@slickgrid-universal/excel-export';
 import { Slicker, type SlickVanillaGridBundle } from '@slickgrid-universal/vanilla-bundle';
 
 import { ExampleGridOptions } from './example-grid-options.js';
 import './example37.scss';
 
-const NB_ITEMS = 100;
+const NB_ITEMS = 1000;
 
 export default class Example37 {
   protected _eventHandler: SlickEventHandler;
 
-  columnDefinitions: Column[] = [];
-  dataset: any[] = [];
-  gridOptions!: GridOption;
-  gridContainerElm: HTMLDivElement;
-  sgb: SlickVanillaGridBundle;
+  gridOptions1!: GridOption;
+  gridOptions2!: GridOption;
+  columnDefinitions1!: Column[];
+  columnDefinitions2!: Column[];
+  dataset1!: any[];
+  dataset2!: any[];
+  sgb1!: SlickVanillaGridBundle;
+  sgb2!: SlickVanillaGridBundle;
 
   attached() {
     this._eventHandler = new SlickEventHandler();
 
     // define the grid options & columns and then create the grid itself
-    this.defineGrid();
+    this.defineGrids();
 
     // mock some data (different in each dataset)
-    this.dataset = this.getData(NB_ITEMS);
-    this.gridContainerElm = document.querySelector<HTMLDivElement>('.grid37') as HTMLDivElement;
-    this.sgb = new Slicker.GridBundle(
-      document.querySelector('.grid37') as HTMLDivElement,
-      this.columnDefinitions,
-      { ...ExampleGridOptions, ...this.gridOptions },
-      this.dataset
+    this.dataset1 = this.getData(NB_ITEMS);
+    this.dataset2 = this.getData(NB_ITEMS);
+
+    this.sgb1 = new Slicker.GridBundle(
+      document.querySelector('.grid37-1') as HTMLDivElement,
+      this.columnDefinitions1,
+      { ...ExampleGridOptions, ...this.gridOptions1 },
+      this.dataset1
     );
-    document.body.classList.add('salesforce-theme');
+    this.sgb2 = new Slicker.GridBundle(
+      document.querySelector('.grid37-2') as HTMLDivElement,
+      this.columnDefinitions2,
+      {
+        ...ExampleGridOptions,
+        ...this.gridOptions2,
+      },
+      this.dataset2
+    );
 
-    const cellSelectionModel = new SlickHybridSelectionModel({ selectActiveRow: true, rowSelectColumnIdArr: ['selector'] });
-    this.sgb.slickGrid?.setSelectionModel(cellSelectionModel);
+    document.body.classList.add('material-theme');
 
-    this.sgb.slickGrid?.onAddNewRow.subscribe((_e, args) => {
-      const item = args.item;
-      this.sgb.slickGrid?.invalidateRow(this.dataset.length);
-      this.dataset.push(item);
-      this.sgb.slickGrid?.updateRowCount();
-      this.sgb.slickGrid?.render();
+    // bind any of the grid events, e.g. onSelectedRangesChanged to show selection range on screen
+    const cellSelectionModel1 = this.sgb1.slickGrid!.getSelectionModel()!;
+    const cellSelectionModel2 = this.sgb2.slickGrid!.getSelectionModel()!;
+    this._eventHandler.subscribe(cellSelectionModel1.onSelectedRangesChanged, (_e, args) => {
+      const targetRange = document.querySelector('#selectionRange') as HTMLSpanElement;
+      targetRange.textContent = '';
+      for (const slickRange of args) {
+        targetRange.textContent += JSON.stringify(slickRange);
+      }
     });
-    this.sgb.slickGrid?.onDragReplaceCells.subscribe(SlickSelectionUtils.defaultCopyDraggedCellRange);
+    this._eventHandler.subscribe(cellSelectionModel2.onSelectedRangesChanged, (_e, args) => {
+      const targetRange = document.querySelector('#selectionRange') as HTMLSpanElement;
+      targetRange.textContent = '';
+      for (const slickRange of args) {
+        targetRange.textContent += JSON.stringify(slickRange);
+      }
+    });
+
+    this.sgb1.slickGrid?.setSelectionModel(new SlickHybridSelectionModel({ selectActiveRow: true, rowSelectColumnIdArr: ['id'] }));
+    this.sgb2.slickGrid?.setSelectionModel(
+      new SlickHybridSelectionModel({ selectActiveRow: false, rowSelectColumnIdArr: ['id', '_checkbox_selector'] })
+    );
   }
 
   dispose() {
     this._eventHandler.unsubscribeAll();
-    this.sgb?.dispose();
-    this.gridContainerElm.remove();
-    document.body.classList.remove('salesforce-theme');
+    this.sgb1?.dispose();
+    this.sgb2?.dispose();
+    document.body.classList.remove('material-theme');
   }
 
   /* Define grid Options and Columns */
-  defineGrid() {
-    this.columnDefinitions = [
+  defineGrids() {
+    this.columnDefinitions1 = [
+      { id: 'id', name: '#', field: 'id', width: 32, maxWidth: 40, excludeFromHeaderMenu: true },
+      { id: 'title', name: 'Title', field: 'title', width: 90, cssClass: 'cell-title', editor: { model: Editors.Text } },
+      { id: 'complete', name: '% Complete', field: 'percentComplete', sortable: true, width: 90 },
+      { id: 'start', name: 'Start', field: 'start', type: 'date', sortable: true, formatter: Formatters.dateUs },
+      { id: 'finish', name: 'Finish', field: 'finish', type: 'date', sortable: true, formatter: Formatters.dateUs },
       {
-        id: 'selector',
-        name: '',
-        field: 'num',
-        width: 30,
+        id: 'priority',
+        name: 'Priority',
+        field: 'priority',
+        width: 80,
+        resizable: false,
+        sortable: true,
+        type: 'number',
+        sortComparer: (x, y, direction) => {
+          return (direction ?? 0) * (x === y ? 0 : x > y ? 1 : -1);
+        },
+        formatter: (_row, _cell, value) => {
+          if (!value) {
+            return '';
+          }
+          const count = +(value >= 3 ? 3 : value);
+          return count === 3 ? 'High' : count === 2 ? 'Medium' : 'Low';
+        },
+      },
+      {
+        id: 'effortDriven',
+        name: 'Effort Driven',
+        field: 'effortDriven',
+        cssClass: 'text-center',
+        width: 95,
+        maxWidth: 120,
+        type: 'boolean',
+        sortable: true,
+        exportCustomFormatter: (_row, _cell, value) => (value ? 'Yes' : 'No'),
+        formatter: Formatters.checkmarkMaterial,
       },
     ];
+    this.columnDefinitions2 = [...this.columnDefinitions1];
 
-    for (let i = 0; i < NB_ITEMS; i++) {
-      this.columnDefinitions.push({
-        id: i,
-        name:
-          i < 26
-            ? String.fromCharCode('A'.charCodeAt(0) + (i % 26))
-            : String.fromCharCode('A'.charCodeAt(0) + Math.floor(i / 26) - 1) + String.fromCharCode('A'.charCodeAt(0) + (i % 26)),
-        field: String(i),
-        minWidth: 60,
-        width: 60,
-        editor: { model: Editors.text },
-      });
-    }
-
-    this.gridOptions = {
+    this.gridOptions1 = {
       autoResize: {
         container: '.demo-container',
       },
+      gridHeight: 250,
+      gridWidth: 800,
       enableCellNavigation: true,
       autoEdit: true,
-      autoCommitEdit: true,
       editable: true,
       headerRowHeight: 35,
-      rowHeight: 30,
-      editorNavigateOnArrows: true, // enable editor navigation using arrow keys
+      rowHeight: 40,
+      enableExcelExport: true,
+      excelExportOptions: {
+        exportWithFormatter: true,
+      },
+      externalResources: [new ExcelExportService()],
 
       // when using the ExcelCopyBuffer, you can see what the selection range is
       enableExcelCopyBuffer: true,
       excelCopyBufferOptions: {
-        //   onCopyCells: (e, args: { ranges: SelectedRange[] }) => console.log('onCopyCells', args.ranges),
-        //   onPasteCells: (e, args: { ranges: SelectedRange[] }) => console.log('onPasteCells', args.ranges),
-        //   onCopyCancelled: (e, args: { ranges: SelectedRange[] }) => console.log('onCopyCancelled', args.ranges),
-        onBeforePasteCell: (_e, args) => {
-          // deny the whole first row and the cells C-E of the second row
-          return !(args.row === 0 || (args.row === 1 && args.cell > 2 && args.cell < 6));
-        },
         copyActiveEditorCell: true,
         removeDoubleQuotesOnPaste: true,
         replaceNewlinesWith: ' ',
       },
     };
+    this.gridOptions2 = {
+      ...this.gridOptions1,
+      // you can also enable checkbox selection & row selection, make sure to use `rowSelectColumnIdArr: ['id', '_checkbox_selector']`
+      enableCheckboxSelector: true,
+      enableRowSelection: true,
+      rowSelectionOptions: {
+        // True (Single Selection), False (Multiple Selections)
+        selectActiveRow: false,
+      },
+    };
   }
 
+  // mock a dataset
   getData(itemCount: number) {
-    // mock a dataset
-    const datasetTmp: any[] = [];
+    const data: any[] = [];
     for (let i = 0; i < itemCount; i++) {
-      const d: any = (datasetTmp[i] = {});
-      d['id'] = i;
-      d['num'] = i;
-    }
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor(Math.random() * 29);
+      const randomFinishYear = randomYear + Math.floor(Math.random() * 10);
+      const randomFinish = new Date(randomFinishYear, randomMonth + 1, randomDay);
 
-    return datasetTmp;
+      data[i] = {
+        id: i,
+        title: 'Task ' + i,
+        duration: Math.floor(Math.random() * 25) + ' days',
+        percentComplete: Math.floor(Math.random() * 100),
+        start: new Date(randomYear, randomMonth, randomDay, randomDay),
+        finish: randomFinish,
+        priority: i % 3 ? 2 : i % 5 ? 3 : 1,
+        effortDriven: i % 4 === 0,
+      };
+    }
+    return data;
   }
 }
