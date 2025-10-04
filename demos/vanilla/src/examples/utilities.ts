c2b9c549e29f98e4cef9164bf7f1ccf9d04e727f
ghiscoding <gbeaulac@gmail.com>
2025-10-04 00:36:58 -0400

    chore: use native popover to show custom toast

--- demos/vanilla/src/examples/utilities.ts
+++ demos/vanilla/src/examples/utilities.ts
@@ -29,15 +29,33 @@ export function randomNumber(min: number, max: number, floor = true) {
 
 export function showToast(msg: string, type: 'danger' | 'info' | 'warning', time = 2000) {
   const div = document.createElement('div');
+  div.setAttribute('popover', '');
   div.className = `notification is-light is-${type} is-small is-narrow toast`;
+  div.style.display = 'block';
   div.style.position = 'absolute';
-  div.style.left = '50%';
-  div.style.top = '20px';
-  div.style.transform = 'translate(-50%)';
   div.style.zIndex = '999';
   div.textContent = msg;
   document.body.appendChild(div);
 
+  // When popover is supported, use it to display the message.
+  // Baseline 2024: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/showPopover
+  if (typeof div.showPopover === 'function') {
+    div.style.margin = '0 auto';
+    div.style.marginTop = '20px';
+    div.style.borderWidth = '0px';
+    div.showPopover();
+    setTimeout(() => {
+      div.hidePopover();
+      div.remove();
+    }, time);
+    return;
+  }
+
+  // @deprecated, remove fallback in next major release
+  // otherwise, fallback (when popover is not supported): keep the div visible as regular HTML and remove after timeout.
+  div.style.left = '50%';
+  div.style.top = '20px';
+  div.style.transform = 'translateX(-50%)';
   setTimeout(() => div.remove(), time);
 }
 
