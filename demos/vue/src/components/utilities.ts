cfdb4f80e15aa2559af14c347d06a527b895d64e
Ghislain B. <gbeaulac@gmail.com>
2025-10-04 00:48:55 -0400

    chore: use native popover to show custom toast (#2183)

--- demos/vue/src/components/utilities.ts
+++ demos/vue/src/components/utilities.ts
@@ -5,25 +5,37 @@ export function randomNumber(min: number, max: number, floor = true) {
 
 export function showToast(msg: string, type: 'danger' | 'info' | 'warning', time = 2000) {
   const divContainer = document.createElement('div');
+  divContainer.setAttribute('popover', '');
   divContainer.className = `toast align-items-center text-bg-${type} border-0`;
   divContainer.style.position = 'absolute';
-  divContainer.style.left = '50%';
-  divContainer.style.top = '20px';
-  divContainer.style.transform = 'translate(-50%)';
   divContainer.style.zIndex = '9999';
-  divContainer.style.display = 'block';
-
-  const divFlex = document.createElement('div');
-  divFlex.className = 'd-flex align-items-center';
 
   const divBody = document.createElement('div');
   divBody.className = 'toast-body';
   divBody.textContent = msg;
-
-  divContainer.appendChild(divFlex);
-  divFlex.appendChild(divBody);
+  divContainer.appendChild(divBody);
   document.body.appendChild(divContainer);
 
+  // When popover is supported, use it to display the message.
+  // Baseline 2024: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/showPopover
+  if (typeof divContainer.showPopover === 'function') {
+    divContainer.style.display = 'block';
+    divContainer.style.margin = '0 auto';
+    divContainer.style.marginTop = '20px';
+    divContainer.style.borderWidth = '0px';
+    divContainer.showPopover();
+    setTimeout(() => {
+      divContainer.hidePopover();
+      divContainer.remove();
+    }, time);
+    return;
+  }
+
+  // @deprecated, remove fallback in next major release
+  // otherwise, fallback (when popover is not supported): keep the div visible as regular HTML and remove after timeout.
+  divContainer.style.left = '50%';
+  divContainer.style.top = '20px';
+  divContainer.style.transform = 'translateX(-50%)';
   setTimeout(() => divContainer.remove(), time);
 }
 
