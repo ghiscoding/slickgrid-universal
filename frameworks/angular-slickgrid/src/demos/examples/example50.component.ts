ae99018ed553d52b1e7cb360db39cb6f2ca5ae47
ghiscoding <gbeaulac@gmail.com>
2026-01-30 20:51:38 -0500

    chore: add missing type import

--- frameworks/angular-slickgrid/src/demos/examples/example50.component.ts
+++ frameworks/angular-slickgrid/src/demos/examples/example50.component.ts
@@ -2,10 +2,10 @@ import { Component, type OnInit } from '@angular/core';
 import {
   AngularSlickgridModule,
   Formatters,
-  OnClickEventArgs,
   type AngularGridInstance,
   type Column,
   type GridOption,
+  type OnClickEventArgs,
 } from '../../library';
 
 export interface Customer {
