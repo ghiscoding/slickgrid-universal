ea146a2663dea52336f8988104a552fe5981a168
ghiscoding <gbeaulac@gmail.com>
2026-02-02 10:30:39 -0500

    chore: fix failing Cypress test

--- frameworks/angular-slickgrid/src/demos/examples/example23.component.ts
+++ frameworks/angular-slickgrid/src/demos/examples/example23.component.ts
@@ -180,9 +180,8 @@ export class Example23Component implements OnInit, OnDestroy {
       },
     ];
 
-    const today = new Date();
     const presetLowestDay = format(addDay(new Date(), -2), 'YYYY-MM-DD');
-    const presetHighestDay = format(addDay(new Date(), today.getDate() < 14 ? 28 : 25), 'YYYY-MM-DD');
+    const presetHighestDay = format(addDay(new Date(), 25), 'YYYY-MM-DD');
 
     this.gridOptions = {
       autoResize: {
