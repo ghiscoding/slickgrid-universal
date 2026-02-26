ddf89f5672a66ebbb2cf8bfcd481ddf12641c422
Ghislain B. <gbeaulac@gmail.com>
2026-02-25 22:04:55 -0500

    chore: always display Header Menu buttons for demo it (#2403)
    * chore: always display Header Menu buttons for demo it

--- demos/vue/src/components/Example08.vue
+++ demos/vue/src/components/Example08.vue
@@ -260,15 +260,19 @@ function vueGridReady(grid: SlickgridVueInstance) {
 </template>
 
 <style lang="scss" scoped>
+// remove "scoped" to see the updated Header Menu since this required a global CSS change and isn't scoped to the component
 @use '@slickgrid-universal/common/dist/styles/sass/slickgrid-theme-bootstrap.scss' with (
-  $slick-header-menu-button-border-width: 0px 1px,
   $slick-header-menu-button-icon-svg-path:
     'M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z',
-  $slick-header-menu-button-icon-size: 16px,
-  $slick-header-menu-button-padding: 10px 0 0 3px,
-  $slick-sort-indicator-hint-opacity: 0
+  $slick-header-menu-button-icon-size: 16px
 );
 
+.grid8 {
+  --slick-header-menu-display: inline-block;
+  // .slick-header-menu-button {
+  //   margin-top: 8px;
+  // }
+}
 blue {
   color: rgb(73, 73, 255);
 }
